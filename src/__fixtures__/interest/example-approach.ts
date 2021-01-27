97d196d88cc30561c796129b5a8aa39e72191105
Adrian <lupescua@gmail.com>
2021-01-27 16:18:54 +0100

    wanted the component to have a selected prop

--- src/__fixtures__/interest/example-approach.ts
+++ src/__fixtures__/interest/example-approach.ts
@@ -6,4 +6,5 @@ export const ExampleApproach: Interest = {
   id: 'f4d489a7-5744-4d4f-8893-3647b77a5a58',
   interestTypeId: ExampleInterestType.id,
   name: 'Example Approach',
+  selected: false,
 }
