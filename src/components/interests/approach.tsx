59ebd14bd412ca8c826dbb062ab7cd7c56a3488f
Adrian <lupescua@gmail.com>
2021-01-27 16:17:44 +0100

    WIP: wanted to make a chip that can be clicked.

--- src/components/interests/approach.tsx
+++ src/components/interests/approach.tsx
@@ -1,12 +1,31 @@
-import React, { FC } from 'react'
-import { Chip } from '@material-ui/core'
+import { Interest } from '@generated/type-graphql';
+import { Chip } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import React, { FC } from 'react';
 
-import { Interest } from '@generated/type-graphql'
-
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    chip: {
+      margin: theme.spacing(0.5),
+    },
+  }),
+);
 export interface ApproachProps {
   interest: Interest
 }
 
 export const Approach: FC<ApproachProps> = ({ interest }) => {
-  return <Chip label={interest.name} />
+  const classes = useStyles();
+  const [color, setChipColors] = React.useState('default');
+
+  const handleClick = (event: React.setOnClickListener, []) => {
+    setChipColors('primary');
+  };
+  return <Chip
+    clickable
+    color={color}
+    className={classes.chip}
+    label={interest.name}
+    onClick={handleClick}
+  />
 }
