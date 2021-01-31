16e83872fa5bfaa07e42f7fc18ea16284517a489
Damon Vestervand <damon@vestervand.net>
2021-01-31 22:40:02 +0100

    Refactor dropdown menu

--- src/components/profile/profile-button.tsx
+++ src/components/profile/profile-button.tsx
@@ -1,17 +1,9 @@
-import React, { MouseEvent } from 'react'
+import React, { FC } from 'react'
 import { IconButton } from '@material-ui/core'
 import { AccountCircle } from '@material-ui/icons'
 
-export interface ProfileButtonProps
-  extends React.HTMLAttributes<HTMLButtonElement> {
-  /**
-   * Action to perform on click
-   */
-  onClick: (MouseEvent) => void
-}
-
-export const ProfileButton = ({ onClick }: ProfileButtonProps) => (
-  <IconButton color="inherit" onClick={onClick} aria-label="profile-button">
+export const ProfileButton: FC = () => (
+  <IconButton color="inherit" aria-label="profile-button">
     <AccountCircle />
   </IconButton>
 )
