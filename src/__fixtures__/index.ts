3899c5282a73fb4cc8eac745d609c22db114b898
Damon Vestervand <damon@vestervand.net>
2021-02-01 23:47:38 +0100

    Add all interests fixture

--- src/__fixtures__/index.ts
+++ src/__fixtures__/index.ts
@@ -1,2 +1,4 @@
 export { ExampleApproach } from 'src/__fixtures__/interest/example-approach'
 export { ExampleInterestType } from 'src/__fixtures__/interest/example-interest-type'
+
+export { Interests } from 'src/__fixtures__/interest/interests'
---
+++ src/__fixtures__/interest/interests.ts
@@ -0,0 +1,423 @@
+export const Interests = {
+  data: {
+    interestTypes: [
+      {
+        id: 'cfeb9e13-d53d-42b2-b753-5592f9de452e',
+        name: 'Focus',
+        parentInterestTypeId: '33d9b417-c793-4610-af5e-6e67fc5329d4',
+        Interests: [],
+        __typename: 'InterestType',
+      },
+      {
+        id: '003cc5da-1af9-4fc5-ab6c-e4f3cb99e42e',
+        name: 'Approach',
+        parentInterestTypeId: '33d9b417-c793-4610-af5e-6e67fc5329d4',
+        Interests: [
+          {
+            id: '3d398126-18c4-4c69-a8a2-917b30bf7479',
+            name: 'Conferences',
+            __typename: 'Interest',
+          },
+          {
+            id: '0fe0bcb3-d227-4c8d-b59d-afb5bead8f83',
+            name: 'Collaboration',
+            __typename: 'Interest',
+          },
+          {
+            id: 'b425275d-8ee6-4e69-8c2d-c46cd22e938b',
+            name: 'Mobilising',
+            __typename: 'Interest',
+          },
+          {
+            id: '7003f091-42e5-4cd0-a67a-1c5123fc08eb',
+            name: 'Debate',
+            __typename: 'Interest',
+          },
+          {
+            id: '6e612e5e-3ca7-4e5f-9291-5ab9a0a53aad',
+            name: 'Demonstrations',
+            __typename: 'Interest',
+          },
+          {
+            id: '02acde79-a777-4e3d-a29d-87dec346c37a',
+            name: 'Policy making',
+            __typename: 'Interest',
+          },
+          {
+            id: 'e13c5097-4227-40ac-9675-fd046526da0f',
+            name: 'Divestment',
+            __typename: 'Interest',
+          },
+          {
+            id: 'b3937767-012d-4347-ba71-4b706cc44b85',
+            name: 'Education',
+            __typename: 'Interest',
+          },
+          {
+            id: 'da31e601-58aa-45c4-99ae-ef8640e25c04',
+            name: 'Research',
+            __typename: 'Interest',
+          },
+          {
+            id: '60892031-dc8d-45ae-94e7-74ad136afae1',
+            name: 'Partnerships',
+            __typename: 'Interest',
+          },
+          {
+            id: '4540c1d7-d5a4-4605-84f2-aaa01956a0a2',
+            name: 'Talks',
+            __typename: 'Interest',
+          },
+          {
+            id: '7ad3fd8a-4304-4e0d-b788-2e8bc3c1f61c',
+            name: 'Knowledge sharing',
+            __typename: 'Interest',
+          },
+          {
+            id: '1ee625a3-6fd5-4ed5-a97e-d92fb915c83a',
+            name: 'Investment',
+            __typename: 'Interest',
+          },
+          {
+            id: '8f185a48-3096-4114-a920-91ab85002382',
+            name: 'Technology',
+            __typename: 'Interest',
+          },
+          {
+            id: 'e16f8a07-33c5-455f-9083-70d289d33719',
+            name: 'Lobbyism',
+            __typename: 'Interest',
+          },
+          {
+            id: '4205c944-2e3c-4843-8ead-b9f79181b1d2',
+            name: 'Workshops',
+            __typename: 'Interest',
+          },
+          {
+            id: 'b5d3e7d8-046c-4f3e-923f-53a75dba6cad',
+            name: 'Youth involvement',
+            __typename: 'Interest',
+          },
+          {
+            id: '9b3c8b8f-a2d6-4f3a-9303-b8d852999da1',
+            name: 'Petitions',
+            __typename: 'Interest',
+          },
+          {
+            id: '42d8444d-bbf6-4317-b321-a73c30e55d24',
+            name: 'Innovation',
+            __typename: 'Interest',
+          },
+          {
+            id: '3171e0c9-addb-463c-b575-a44c7f724f0c',
+            name: 'Write opinion pieces',
+            __typename: 'Interest',
+          },
+        ],
+        __typename: 'InterestType',
+      },
+      {
+        id: '076c2c59-008d-4bb5-bc7b-a494b034dba0',
+        name: 'Tags',
+        parentInterestTypeId: '33d9b417-c793-4610-af5e-6e67fc5329d4',
+        Interests: [
+          {
+            id: '1af228e7-8259-4fcc-905c-3a356b99258b',
+            name: 'CreativeCircle',
+            __typename: 'Interest',
+          },
+          {
+            id: '8a26eb3c-f7dd-4b52-8bca-f3afe7ea152c',
+            name: 'COP26',
+            __typename: 'Interest',
+          },
+          {
+            id: '17310601-59c0-4f5a-8b69-df618a355df5',
+            name: 'Feminism',
+            __typename: 'Interest',
+          },
+          {
+            id: '48c0eec1-7b6d-4f98-b6cf-b801a112dff7',
+            name: 'Dropoliendan',
+            __typename: 'Interest',
+          },
+          {
+            id: '99cdb176-bb62-4638-8ae7-632238ab02f7',
+            name: 'DoughnutModel',
+            __typename: 'Interest',
+          },
+          {
+            id: '20676fcd-def1-4ab6-a648-3771526f1a60',
+            name: 'GreenFridayAarhus',
+            __typename: 'Interest',
+          },
+          {
+            id: '9766156d-3f34-4092-8ec3-717ca9216a3e',
+            name: 'EuropeanGreenDeal',
+            __typename: 'Interest',
+          },
+          {
+            id: 'ac5dcf0b-040f-46ac-9d85-91a14e3ed8d5',
+            name: 'GreenlightAarhus',
+            __typename: 'Interest',
+          },
+          {
+            id: '38929432-3eb1-4859-9255-739faa80fae9',
+            name: 'UniteTheWorld',
+            __typename: 'Interest',
+          },
+          {
+            id: 'd5e7db57-3f83-4042-affa-6fa177feb026',
+            name: 'GreenNewDeal',
+            __typename: 'Interest',
+          },
+          {
+            id: '02384e92-f578-470a-ba03-1184f85c4470',
+            name: 'ReGeneration2030',
+            __typename: 'Interest',
+          },
+          {
+            id: '78430d53-565a-4119-9204-44e6083da078',
+            name: 'Grøngenstart',
+            __typename: 'Interest',
+          },
+          {
+            id: '195674a0-0c1b-468e-bf6d-d21761caabe6',
+            name: 'Stopbalticpipeline',
+            __typename: 'Interest',
+          },
+          {
+            id: '6365bf68-75cd-4048-9fa9-27b2ac0ad6ce',
+            name: 'GrønneAktørerAarhus',
+            __typename: 'Interest',
+          },
+          {
+            id: 'd3866bd9-a82c-438f-908d-f6f8da10498c',
+            name: 'PostGrowth',
+            __typename: 'Interest',
+          },
+          {
+            id: '054193d2-3e29-4311-b0e3-9746c48cc34c',
+            name: 'MellowDesigns',
+            __typename: 'Interest',
+          },
+          {
+            id: 'e926268c-e0a3-4d0e-b9d3-3a7fbfd0e4c5',
+            name: 'Klimaplan',
+            __typename: 'Interest',
+          },
+          {
+            id: '980b5f58-b857-445d-a441-fc382872013c',
+            name: 'Banfossilfuels',
+            __typename: 'Interest',
+          },
+          {
+            id: '9ba67283-65fe-4dcd-b3d8-9cae37a30190',
+            name: 'BalticSea',
+            __typename: 'Interest',
+          },
+          {
+            id: 'c45eaa9d-892f-462a-801b-05485be57e59',
+            name: 'Kommunalvalg2021',
+            __typename: 'Interest',
+          },
+          {
+            id: 'accaab3a-c638-4f70-8ead-e0fe08e50b61',
+            name: 'SCM',
+            __typename: 'Interest',
+          },
+          {
+            id: '4df17a5f-0d87-47db-b37d-ed2b3b024b1c',
+            name: 'BalticSeaYouthPlatform',
+            __typename: 'Interest',
+          },
+          {
+            id: 'e2d8faa3-8cfb-4901-b2c1-84892cf0356e',
+            name: 'BlackLivesMatter',
+            __typename: 'Interest',
+          },
+          {
+            id: 'f6df82ec-9a82-4828-8780-f3994e7855fa',
+            name: 'AntiRacism',
+            __typename: 'Interest',
+          },
+        ],
+        __typename: 'InterestType',
+      },
+      {
+        id: 'b4d7b0eb-e941-4832-a2c3-e4c2cf3183a1',
+        name: 'Economy',
+        parentInterestTypeId: 'cfeb9e13-d53d-42b2-b753-5592f9de452e',
+        Interests: [
+          {
+            id: 'b585df46-10b4-4b39-b71b-d72fa9a3adcd',
+            name: 'Agriculture',
+            __typename: 'Interest',
+          },
+          {
+            id: 'ff2916bf-0809-45b5-ac85-354dd7f76986',
+            name: 'Consumption',
+            __typename: 'Interest',
+          },
+          {
+            id: 'b4fd4e75-f086-4f1b-a244-8d38ce604a14',
+            name: 'Decent work',
+            __typename: 'Interest',
+          },
+          {
+            id: 'a9ee1727-33a4-461c-b5f5-51d367b25f79',
+            name: 'Economic system',
+            __typename: 'Interest',
+          },
+          {
+            id: 'bff0a5ca-ee4e-436c-b7d9-58c752243b72',
+            name: 'Energy',
+            __typename: 'Interest',
+          },
+          {
+            id: '3f4cef83-e9b7-4b9e-93ca-dc218ea40c20',
+            name: 'Food',
+            __typename: 'Interest',
+          },
+          {
+            id: 'fb6b93a7-63ec-4288-b41f-fa9a9697899f',
+            name: 'Production',
+            __typename: 'Interest',
+          },
+          {
+            id: 'f8e31d76-eeba-48b8-8fd4-32e80dc8327c',
+            name: 'Transportation',
+            __typename: 'Interest',
+          },
+          {
+            id: 'f3edebc4-26cb-4167-bf5d-b284ff2fb315',
+            name: 'Waste',
+            __typename: 'Interest',
+          },
+        ],
+        __typename: 'InterestType',
+      },
+      {
+        id: 'da13b6ad-a878-4b91-9518-99b0902fb8a5',
+        name: 'Environment',
+        parentInterestTypeId: 'cfeb9e13-d53d-42b2-b753-5592f9de452e',
+        Interests: [
+          {
+            id: '25e9303c-d18d-46a4-8be8-3658ff3e633a',
+            name: 'Air',
+            __typename: 'Interest',
+          },
+          {
+            id: '208cd79a-ecc3-4779-bd6f-a1efc71f0522',
+            name: 'Biodiversity land',
+            __typename: 'Interest',
+          },
+          {
+            id: 'c99ff4be-a15d-471e-b799-93234e5f337f',
+            name: 'Biodiversity water',
+            __typename: 'Interest',
+          },
+          {
+            id: '135c0c9b-3027-4185-bde1-cebe50cdb688',
+            name: 'Clean freshwater',
+            __typename: 'Interest',
+          },
+          {
+            id: 'e034a0ce-ab82-41dd-8f39-1f92e031a440',
+            name: 'Clean oceans',
+            __typename: 'Interest',
+          },
+          {
+            id: 'de3f6741-a50b-4e25-b8dd-1212edf7b408',
+            name: 'Climate change',
+            __typename: 'Interest',
+          },
+          {
+            id: '6ee6d692-8ab6-41c5-a671-ec90e2850bd6',
+            name: 'Forest',
+            __typename: 'Interest',
+          },
+          {
+            id: 'fc3b7dc0-c214-4953-8b99-43185129ce18',
+            name: 'Soil',
+            __typename: 'Interest',
+          },
+        ],
+        __typename: 'InterestType',
+      },
+      {
+        id: 'fe8f1a4d-1878-4548-a92d-dc06045f9101',
+        name: 'Social',
+        parentInterestTypeId: 'cfeb9e13-d53d-42b2-b753-5592f9de452e',
+        Interests: [
+          {
+            id: '08068932-465b-4c1c-978e-8fa6688ea7db',
+            name: 'Corruption',
+            __typename: 'Interest',
+          },
+          {
+            id: '232c108c-5cae-4baa-944a-192c90202bd9',
+            name: 'Drinking water',
+            __typename: 'Interest',
+          },
+          {
+            id: '3e1dc829-7b9a-4b4e-99a0-c3200b9deccd',
+            name: 'Education',
+            __typename: 'Interest',
+          },
+          {
+            id: '516eb4d4-2950-4f86-a2f5-f2af70608f66',
+            name: 'Gender Equality',
+            __typename: 'Interest',
+          },
+          {
+            id: 'eabf5001-d58f-4057-9386-30b7dcdb74f2',
+            name: 'Health',
+            __typename: 'Interest',
+          },
+          {
+            id: '2cb2804a-c9c1-44ea-bfad-b237993a6bcf',
+            name: 'Housing',
+            __typename: 'Interest',
+          },
+          {
+            id: 'b213b711-a087-4b69-9589-c933b985bf72',
+            name: 'Hunger',
+            __typename: 'Interest',
+          },
+          {
+            id: '74b0d391-0145-4af4-8826-55985d5a5994',
+            name: 'Indigenous people',
+            __typename: 'Interest',
+          },
+          {
+            id: '46ea0ae6-3b90-4757-bfef-5069938bf59d',
+            name: 'Inequality',
+            __typename: 'Interest',
+          },
+          {
+            id: '22444cbc-c5d8-4ea5-a72d-b4b40fe054cf',
+            name: 'Poverty',
+            __typename: 'Interest',
+          },
+          {
+            id: 'b21c53fa-8ac0-46a5-af2d-d79e42bd0353',
+            name: 'Refugees',
+            __typename: 'Interest',
+          },
+          {
+            id: '4207733e-e231-445d-9811-6f2a2183eb33',
+            name: 'Urban planning',
+            __typename: 'Interest',
+          },
+          {
+            id: 'cfd75c11-baaf-44a6-bc48-3db55d23d264',
+            name: 'Violence & Abuse',
+            __typename: 'Interest',
+          },
+        ],
+        __typename: 'InterestType',
+      },
+    ],
+  },
+}
