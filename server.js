5559e467765db548174eff382653adc950bf27de
Damon Vestervand <damon@vestervand.net>
2021-02-09 00:49:25 +0100

    Set correct app config for custom server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev, dir: './src' })
+const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
