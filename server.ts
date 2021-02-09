34a4ef9290f73348562c3bc0bc7befa7ad983948
Damon Vestervand <damon@vestervand.net>
2021-02-09 22:21:30 +0100

    More tinkering, add basic auth

---
+++ server.ts
@@ -0,0 +1,39 @@
+require('dotenv').config()
+
+import express, { Request, Response } from 'express'
+import next from 'next'
+import basicAuth from 'express-basic-auth'
+import cryptoRandomString from 'crypto-random-string'
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+const port = process.env.PORT || 3000
+
+;(async () => {
+  try {
+    await app.prepare()
+    const server = express()
+    server.use(
+      basicAuth({
+        users: {
+          admin:
+            process.env.BASIC_AUTH_ADMIN_PASS ||
+            cryptoRandomString({ length: 18, type: 'base64' }),
+        },
+        challenge: true,
+        realm: cryptoRandomString({ length: 10 }),
+      })
+    )
+    server.all('*', (req: Request, res: Response) => {
+      return handle(req, res)
+    })
+    server.listen(port, (err?: any) => {
+      if (err) throw err
+      console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`)
+    })
+  } catch (e) {
+    console.error(e)
+    process.exit(1)
+  }
+})()
