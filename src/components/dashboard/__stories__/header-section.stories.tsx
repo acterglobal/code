57b4e7208c4197333a9a8801cf65fc59c9cabc41
Malik <malik43213@gmail.com>
2021-02-15 10:19:38 +0100

    added Acter props type

--- src/components/dashboard/__stories__/header-section.stories.tsx
+++ src/components/dashboard/__stories__/header-section.stories.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { Meta } from '@storybook/react'
 import HeaderSection from '../header-section'
 import { ExampleActer } from '../../../__fixtures__/acter/example-acter'
+import { Acter } from '@generated/type-graphql'
 
 export default {
   title: 'landingpage/Header',
   component: HeaderSection,
 } as Meta
 
-export const Header: React.FC = () => <HeaderSection acter={ExampleActer} />
+export const Header: React.FC<Acter> = () => (
+  <HeaderSection acter={ExampleActer} />
+)
