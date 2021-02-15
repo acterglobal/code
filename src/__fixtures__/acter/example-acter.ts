4c3824fa9c614d24b7601cceac8ebd3793bf161e
Malik <44589493+malik-shaik@users.noreply.github.com>
2021-02-15 15:49:12 +0100

    Add banner section 4/4 (#25)
    * add banner section
    
    * add header section for landing page
    
    * refactor code
    
    * some changes
    
    * Add schema change, DB migration for Acter avatar & banner images
    
    * added todo
    
    * updated example-acter in fixture
    
    * added Acter props type
    
    * added props and refactored
    
    * added the tests for header section
    
    Co-authored-by: Damon Vestervand <damon@vestervand.net>

--- src/__fixtures__/acter/example-acter.ts
+++ src/__fixtures__/acter/example-acter.ts
@@ -6,8 +6,16 @@ import { ExampleUser } from '__fixtures__'
 export const ExampleActer: Acter = {
   id: '9a64149c-5641-4841-96b1-1b2ec85f88aa',
   acterTypeId: OrganizationActerType.id,
-  name: 'My Organization',
+  name: 'Greenlight Aarhus',
+  location: 'Aarhus Denmark',
   slug: 'my-organization',
+  description: `Lorem ipsum dolor sit amet consectetur adipisicing
+  elit. Quam laudantium quas voluptates assumenda deserunt, sequi
+  alias veritatis vitae eum culpa amet delectus eveniet tempore
+  quibusdam repellat ut? Corrupti, consequuntur ipsam`,
+  url: 'company url',
+  avatarUrl: `https://res.cloudinary.com/dfglnmgmx/image/upload/v1612778108/acter/acter-logo-144.png`,
+  bannerUrl: `https://res.cloudinary.com/dfglnmgmx/image/upload/v1612781078/acter/top-banner.png`,
   createdAt: new Date(),
   updatedAt: new Date(),
   createdByUserId: ExampleUser.id,
---
+++ src/components/dashboard/header-section.tsx
@@ -0,0 +1,103 @@
+import React, { FC } from 'react'
+import {
+  makeStyles,
+  createStyles,
+  withStyles,
+  Theme,
+} from '@material-ui/core/styles'
+import { Box, Typography as MUIT, Button } from '@material-ui/core'
+import Image from 'next/image'
+import Avatar from '../profile/avatar'
+import { Acter } from '@generated/type-graphql'
+
+// ? overriding the MaterialUI tab styles
+const Typography = withStyles((theme: Theme) =>
+  createStyles({
+    h4: {
+      fontWeight: 'bold',
+    },
+  })
+)(MUIT)
+
+//  ? custom styles
+const useStyles = makeStyles((theme: Theme) => ({
+  bannerSection: {
+    position: 'relative',
+  },
+  infoSection: {
+    display: 'flex',
+    // backgroundColor: 'blue',
+    height: '80px',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  info: {
+    margin: '0px',
+    marginLeft: '190px',
+  },
+  title: {
+    fontSize: '1.4rem',
+  },
+  location: {
+    color: theme.palette.secondary.dark,
+  },
+  joinBtn: {
+    borderRadius: 25,
+    width: '100px',
+    backgroundColor: theme.palette.primary.main,
+    color: 'white',
+    marginRight: '40px',
+    textTransform: 'none',
+  },
+}))
+
+export interface HeaderSectionProps {
+  acter: Acter
+}
+
+const HeaderSection: FC<HeaderSectionProps> = ({ acter }) => {
+  const classes = useStyles()
+  console.log(acter)
+  return (
+    <Box className={classes.bannerSection}>
+      <Image
+        loader={() => acter.bannerUrl}
+        src={'/acter-logo-144.png'}
+        alt="Acter Logo"
+        layout="intrinsic"
+        height={400}
+        width={1920}
+      />
+      <Box className={classes.infoSection}>
+        <Avatar imageUrl={acter.avatarUrl} />
+
+        <Box className={classes.info}>
+          <Typography role="acter-name" variant="h4" className={classes.title}>
+            {acter.name}
+          </Typography>
+          <Typography
+            role="acter-location"
+            variant="subtitle2"
+            className={classes.location}
+          >
+            {acter.location}
+          </Typography>
+        </Box>
+        <Box>
+          <Button
+            className={classes.joinBtn}
+            variant="contained"
+            size="medium"
+            disableElevation
+            onClick={() => console.log('join button clicked')}
+          >
+            Join
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+export default HeaderSection
