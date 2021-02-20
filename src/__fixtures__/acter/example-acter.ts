47e9ffd03ed2531265ff06f1e1f1c7fa32efa882
Damon <damon@uber.com>
2021-02-20 10:49:03 +0100

    Add URL helper for Acters (#38)
    * Add URL helper for Acters

--- src/__fixtures__/acter/example-acter.ts
+++ src/__fixtures__/acter/example-acter.ts
@@ -6,6 +6,7 @@ import { ExampleUser } from '__fixtures__'
 export const ExampleActer: Acter = {
   id: '9a64149c-5641-4841-96b1-1b2ec85f88aa',
   acterTypeId: OrganizationActerType.id,
+  ActerType: OrganizationActerType,
   name: 'Greenlight Aarhus',
   location: 'Aarhus Denmark',
   slug: 'my-organization',
