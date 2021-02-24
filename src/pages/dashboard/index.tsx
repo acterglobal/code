a3d671a153d13bafefc06356912d5eea2c8c763d
Damon <damon@vestervand.net>
2021-02-24 22:49:55 +0100

    Remove duplicate dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import { NextPage } from 'next'
+import { Acter, User } from '@generated/type-graphql'
 
 import { composeProps, ComposedGetServerSideProps } from 'lib/compose-props'
+import { getToken, getUserProfile, getFollowedActers } from 'src/props'
 
-const DashboardPage: NextPage = () => {
-  return <div></div>
+import { Layout } from 'src/components/layout'
+
+import { Dashboard } from 'src/components/dashboard'
+
+interface DashboardPageProps {
+  followingActers: Acter[]
+  user: User
+}
+
+const DashboardPage: NextPage<DashboardPageProps> = ({
+  followingActers,
+  user,
+}) => {
+  return (
+    <Layout loggedInUser={user}>
+      <Dashboard acters={followingActers} />
+    </Layout>
+  )
 }
 
 export const getServerSideProps: ComposedGetServerSideProps = (ctx) =>
-  composeProps(ctx)
+  composeProps(ctx, getToken, getUserProfile, getFollowedActers)
 
 export default DashboardPage
