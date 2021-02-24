64e70e2763982f153bd9df10fac5f6302a192103
Damon <damon@vestervand.net>
2021-02-24 22:48:10 +0100

    Add function for getting user initials

---
+++ src/lib/get-initials/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Turn a name into initials
+ * @param name string to be turned into initials
+ * @param count number of initials. Default is 2
+ * @returns a string with the first X initials of the passed name
+ */
+export const getInitials = (name: string, count: number = 2) => {
+  return name
+    .split(' ')
+    .slice(0, count)
+    .reduce((prev, cur) => `${prev}${cur.substr(0, 1)}`, '')
+}
