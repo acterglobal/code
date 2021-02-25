3fdb394172983d9d14e04cf481a2f42825891a36
Damon <damon@vestervand.net>
2021-02-25 10:03:22 +0100

    lint fix

--- src/lib/get-initials/index.ts
+++ src/lib/get-initials/index.ts
@@ -4,7 +4,7 @@
  * @param count number of initials. Default is 2
  * @returns a string with the first X initials of the passed name
  */
-export const getInitials = (name: string, count: number = 2) => {
+export const getInitials = (name: string, count = 2): string => {
   return name
     .split(' ')
     .slice(0, count)
