8f8eb48a8adcb971411fdb8307cd98668355fb6f
Malik <malik43213@gmail.com>
2021-02-25 14:00:57 +0100

    add example-activity

---
+++ src/__fixtures__/activity/example-activity.ts
@@ -0,0 +1,19 @@
+import { ExampleUser } from '__fixtures__'
+
+export const ExampleActiivity = {
+  id: '9a64149c-5641-4841-96b1-1b2ec85f88aa',
+  acterId: '9a64149c-5641-4841-96b1-1b2ec85f99bb',
+  title: 'Green eco talk',
+  description: `This section about this activity. This section about this activityThis section about this activityThis section about this activityThis section about this activityThis section about this activityThis section about this activityThis section about this activityThis section about this activityThis section about this activityThis section about this activityThis section about this activity This section about this activity This section about this activity This section about this activity`,
+  startDate: '19 MAR. 17.00',
+  endDate: '20 MAR. 18.00',
+  //   location: 'online',
+  location: 'Fiskerivej 2H, 8000 Aarhus',
+  interests: [],
+  people: [],
+  organizations: [],
+  image: 'https://acter.ams3.cdn.digitaloceanspaces.com/assets/tree.png',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  createdByUserId: ExampleUser.id,
+}
--- src/components/acter/landing-page/activities/activity-details.tsx
+++ src/components/acter/landing-page/activities/activity-details.tsx
@@ -1,21 +1,66 @@
-import React from 'react'
+import React, { FC } from 'react'
 import Image from 'next/image'
 import { Modal } from 'src/components/util/modal/modal'
+import { ExampleActiivity } from 'src/__fixtures__/activity/example-activity'
+import { Box, Typography } from '@material-ui/core'
+import { makeStyles, Theme } from '@material-ui/core/styles'
+import { green, grey } from '@material-ui/core/colors'
 
-const myLoader = ({ src, width, quality }) => {
-  return `https://acter.ams3.cdn.digitaloceanspaces.com/assets/${src}`
-}
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    width: 750,
+    backgroundColor: '#F2F2F2',
+    borderRadius: '10px 10px 0px 0px',
+  },
+  image: {
+    borderRadius: '10px 10px 0px 0px',
+  },
+  activityInfo: {
+    padding: 20,
+    height: 120,
+  },
+  date: {
+    color: green[500],
+    fontWeight: 'bolder',
+    fontSize: '0.9rem',
+  },
+  title: {
+    fontSize: '1.4rem',
+    fontWeight: 'bold',
+  },
+  location: {
+    color: grey[800],
+    fontSize: '0.9rem',
+    fontWeight: 'lighter',
+  },
+}))
 
 export const ActivityDetails = () => {
+  const classes = useStyles()
+  const activity = { ...ExampleActiivity }
+
   return (
     <Modal>
-      <Image
-        loader={myLoader}
-        src={`organisation-logo.png`}
-        alt="Picture of the author"
-        width={500}
-        height={500}
-      />
+      <Box className={classes.container}>
+        <Image
+          className={classes.image}
+          src={activity.image}
+          alt="Picture of activity"
+          width={750}
+          height={250}
+        />
+        <Box className={classes.activityInfo}>
+          <Typography className={classes.date} variant="subtitle1">
+            {`${activity.startDate} - ${activity.endDate}`}
+          </Typography>
+          <Typography className={classes.title} variant="h3">
+            {activity.title}
+          </Typography>
+          <Typography className={classes.location} variant="body2">
+            {activity.location}
+          </Typography>
+        </Box>
+      </Box>
     </Modal>
   )
 }
