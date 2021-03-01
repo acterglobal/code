d538b850120c816400a89089a83edd1f34ffef5a
Malik <malik43213@gmail.com>
2021-03-01 10:05:02 +0100

    add responsive font sizes to theme

--- src/themes/acter-theme.tsx
+++ src/themes/acter-theme.tsx
@@ -1,13 +1,15 @@
 import { ThemeProvider } from '@material-ui/core/styles'
 import { green, grey } from '@material-ui/core/colors'
-import { createMuiTheme } from '@material-ui/core/styles'
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles'
 
-export const acterTheme = createMuiTheme({
+export const theme = createMuiTheme({
   palette: {
     primary: green,
     secondary: grey,
   },
 })
+
+export const acterTheme = responsiveFontSizes(theme)
 
 export const ActerThemeProvider = ({ children }) => (
   <ThemeProvider theme={acterTheme}>{children}</ThemeProvider>
