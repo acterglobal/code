17b70638fcc238d946705660500ccf2d33ed0ce4
Damon <damon@uber.com>
2021-03-04 16:36:34 +0100

    Use Acter Type picker for new Acter create (#57)

---
+++ src/pages/acter/new.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from 'next'
+import { useRouter, NextRouter } from 'next/router'
+import { AddActerType } from 'src/components/acter/add-acter-type'
+import { Layout } from 'src/components/layout'
+
+const _handleClick = (router: NextRouter) => (acterType: string) =>
+  router.push(`/${acterType}s/new`)
+
+export const NewActerTypeSelectPage: NextPage = () => {
+  const router = useRouter()
+  return (
+    <Layout>
+      <AddActerType onClick={_handleClick(router)} />
+    </Layout>
+  )
+}
+
+export default NewActerTypeSelectPage
