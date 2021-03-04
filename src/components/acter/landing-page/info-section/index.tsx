c52549819ee352d79ab2fe6974f15f0fc76bd196
Damon <damon@vestervand.net>
2021-03-04 16:00:16 +0100

    Consolidate on single follower acter component

--- src/components/acter/landing-page/info-section/index.tsx
+++ src/components/acter/landing-page/info-section/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 import { Card, CardContent } from '@material-ui/core'
 import Header from 'src/components/acter/landing-page/info-section/header'
-import { AvatarList } from 'src/components/acter/landing-page/info-section/avatar-list'
+import { FollowersAvatars } from 'src/components/acter/followers-avatars'
 
 import { Acter } from '@generated/type-graphql'
 interface InfoSectionProps {
@@ -9,18 +9,11 @@ interface InfoSectionProps {
 }
 
 export const InfoSection: FC<InfoSectionProps> = ({ acter }) => {
-  const users = acter.Followers.filter(
-    ({ Follower }) => Follower.ActerType.name === 'user'
-  ).map(({ Follower }) => Follower)
-  const organizations = acter.Followers.filter(
-    ({ Follower }) => Follower.ActerType.name === 'organization'
-  ).map(({ Follower }) => Follower)
   return (
     <Card variant="outlined">
       <CardContent>
         <Header title={acter.name} description={acter.description} />
-        <AvatarList title="People" acters={users} />
-        <AvatarList title="Organizations" acters={organizations} />
+        <FollowersAvatars acter={acter} />
       </CardContent>
     </Card>
   )
