34e636c691927389756d3f5aed5ce6bd3a8ef536
Malik <malik43213@gmail.com>
2021-03-06 11:30:25 +0100

    few changes

--- src/components/acter/list-by-type.tsx
+++ src/components/acter/list-by-type.tsx
@@ -1,6 +1,9 @@
 import React, { Fragment, FC } from 'react'
 import Link from 'next/link'
 import {
+  Box,
+  Grid,
+  Hidden,
   Link as MuiLink,
   List,
   ListItem,
@@ -10,6 +13,15 @@ import {
 import { acterAsUrl } from 'src/lib/acter/acter-as-url'
 
 import { Acter } from '@generated/type-graphql'
+import { createStyles, withStyles, Theme } from '@material-ui/core/styles'
+
+const StyledBox = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      border: '1px solid black',
+    },
+  })
+)(Box)
 
 export interface ActerListByTypeProps {
   acters: Acter[]
@@ -26,21 +38,49 @@ export const ActerListByType: FC<ActerListByTypeProps> = ({ acters }) => {
   }, {} as ActersByType)
 
   return (
-    <List>
+    // <List>
+    <Grid container>
       {Object.entries(actersByType).map(([type, subset]) => (
-        <ListItem key={`acterType-${type}`} aria-label="acter-type">
-          <List>
-            <ListSubheader role="heading">{type}</ListSubheader>
-            {subset.map((acter) => (
-              <ListItem key={`acter-${acter.id}`}>
-                <Link href={acterAsUrl(acter)} passHref>
-                  <MuiLink>{acter.name}</MuiLink>
-                </Link>
-              </ListItem>
-            ))}
-          </List>
-        </ListItem>
+        <Grid item xs={6} sm={3} style={{ border: '1px solid blue' }}>
+          {subset.map((acter) => (
+            <Grid item key={`acter-${acter.id}`}>
+              <Link href={acterAsUrl(acter)} passHref>
+                <MuiLink>{acter.name}</MuiLink>
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+        // <Hidden xsDown>
+        //   <Grid item xs={6} sm={3} style={{ border: '1px solid red' }}>
+        //     one
+        //   </Grid>
+        // </Hidden>
+        // <Grid item xs={6} sm={3} style={{ border: '1px solid blue' }}>
+        //   two
+        // </Grid>
+        // <Hidden xsDown>
+        //   <Grid item xs={6} sm={3} style={{ border: '1px solid green' }}>
+        //     three
+        //   </Grid>
+        // </Hidden>
+        // <Grid item xs={6} sm={3} style={{ border: '1px solid yellow' }}>
+        //   four
+        // </Grid>
+        // <ListItem key={`acterType-${type}`} aria-label="acter-type">
+        //   <List>
+        //     <ListSubheader role="heading">{type}</ListSubheader>
+        //     {subset.map((acter) => (
+        //       <ListItem key={`acter-${acter.id}`}>
+        //         <Link href={acterAsUrl(acter)} passHref>
+        //           <MuiLink>{acter.name}</MuiLink>
+        //         </Link>
+        //       </ListItem>
+        //     ))}
+        //   </List>
+        // </ListItem>
       ))}
-    </List>
+    </Grid>
+
+    // </List>
   )
 }
