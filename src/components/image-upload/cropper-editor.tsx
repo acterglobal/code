a262facf81b5f45267d9f529190817a765a538b3
Malik <44589493+malik-shaik@users.noreply.github.com>
2021-03-07 13:54:30 +0100

    added edit tools and modal to image cropper (#69)

--- src/components/image-upload/cropper-editor.tsx
+++ src/components/image-upload/cropper-editor.tsx
@@ -1,22 +1,56 @@
 import React, { FC, useState } from 'react'
-import { Crop as CropIcon } from '@material-ui/icons'
+import {
+  Crop as CropIcon,
+  RotateLeft as RotateLeftIcon,
+  RotateRight as RotateRightIcon,
+  ZoomIn as ZoomInIcon,
+  ZoomOut as ZoomOutIcon,
+} from '@material-ui/icons'
 import { makeStyles, Theme } from '@material-ui/core/styles'
-import { Button } from '@material-ui/core/'
+import { Box, Button } from '@material-ui/core/'
 import Cropper from 'react-cropper'
 import 'cropperjs/dist/cropper.css'
+import { Modal } from 'src/components/util/modal/modal'
+import { green, grey } from '@material-ui/core/colors'
 
 const useStyles = makeStyles((theme: Theme) => ({
   container: {
-    width: 250,
-    top: 0,
-    left: 0,
-    position: 'absolute',
+    width: theme.spacing(55),
+    height: theme.spacing(50),
+    padding: theme.spacing(1),
+    backgroundColor: 'white',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cropper: {
+    width: theme.spacing(54),
+    height: theme.spacing(45),
+  },
+  toolsContainer: {
+    display: 'flex',
+    alignItems: 'center',
   },
   cropBtn: {
     backgroundColor: theme.palette.primary.main,
     color: 'white',
     textTransform: 'none',
-    margin: 10,
+    margin: theme.spacing(1),
+    '&:hover': {
+      backgroundColor: green[400],
+    },
+  },
+  tool: {
+    cursor: 'pointer',
+    width: theme.spacing(4),
+    height: theme.spacing(4),
+    backgroundColor: grey[800],
+    color: 'white',
+    marginLeft: theme.spacing(2),
+    '&:hover': {
+      backgroundColor: grey[700],
+    },
   },
 }))
 
@@ -69,27 +103,53 @@ export const ImageCropper: FC<ImageCropperProps> = (props) => {
     handleCrop(calculateCropData(), cropper.getCroppedCanvas().toDataURL())
 
   return (
-    <div className={classes.container}>
-      <Cropper
-        src={image !== {} ? image : ''}
-        style={{ maxHeight: 200 }}
-        zoomable={true}
-        movable={true}
-        modal={true}
-        aspectRatio={aspectRatio}
-        cropBoxResizable={true}
-        guides={false}
-        onInitialized={(instance) => setCropper(instance)}
-      />
+    <Modal disableBackdropClick={true}>
+      <Box className={classes.container}>
+        <Cropper
+          src={image !== {} ? image : ''}
+          className={classes.cropper}
+          zoomable={true}
+          movable={true}
+          modal={true}
+          aspectRatio={aspectRatio}
+          cropBoxResizable={true}
+          guides={false}
+          rotatable={true}
+          onInitialized={(instance) => setCropper(instance)}
+        />
+        <Box className={classes.toolsContainer}>
+          <Button
+            variant="contained"
+            disableElevation
+            className={classes.cropBtn}
+            onClick={handleImageCrop}
+            endIcon={<CropIcon />}
+          >
+            Crop
+          </Button>
 
-      <Button
-        variant="contained"
-        className={classes.cropBtn}
-        onClick={handleImageCrop}
-        endIcon={<CropIcon />}
-      >
-        Crop
-      </Button>
-    </div>
+          <RotateRightIcon
+            fontSize="inherit"
+            className={classes.tool}
+            onClick={() => cropper.rotate(-90)}
+          />
+          <RotateLeftIcon
+            fontSize="inherit"
+            className={classes.tool}
+            onClick={() => cropper.rotate(90)}
+          />
+          <ZoomInIcon
+            fontSize="inherit"
+            className={classes.tool}
+            onClick={() => cropper.zoom(0.1)}
+          />
+          <ZoomOutIcon
+            fontSize="inherit"
+            className={classes.tool}
+            onClick={() => cropper.zoom(-0.1)}
+          />
+        </Box>
+      </Box>
+    </Modal>
   )
 }
