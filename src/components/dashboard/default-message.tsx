e1f7406a51c5cdde31048260144ac4daa4553601
Malik <44589493+malik-shaik@users.noreply.github.com>
2021-03-08 20:12:34 +0100

    added default messge if no orgs/networks exist (#76)
    * added default messge if no orgs/networks exist
    
    * set default image source
    
    * Revert "set default image source"
    
    * deleted greenlight house link

---
+++ src/components/dashboard/default-message.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react'
+import Link from 'next/link'
+
+import { Box, Typography, Link as MuiLink } from '@material-ui/core'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      marginTop: theme.spacing(8),
+      textAlign: 'center',
+    },
+    link: {
+      cursor: 'pointer',
+    },
+  })
+)
+
+export const DefaultMessage: FC = () => {
+  const classes = useStyles()
+  return (
+    <Box className={classes.root}>
+      <Typography variant="body1">
+        You have not created any Organisations or Networks.
+      </Typography>
+      <Typography variant="body1">
+        Get started{' '}
+        <Link href="/acter/new">
+          <MuiLink className={classes.link}>here</MuiLink>
+        </Link>
+      </Typography>
+    </Box>
+  )
+}
