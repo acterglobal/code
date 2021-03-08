7d19d5407abf5db87da0596ee45f8a883ff0a842
Malik <malik43213@gmail.com>
2021-03-08 17:49:15 +0100

    added default messge if no orgs/networks exist

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { flattenFollowing } from 'src/lib/acter/flatten-following'
 import { ActerListByType } from 'src/components/acter/list-by-type'
+import { DefaultMessage } from 'src/components/dashboard/default-message'
 
 import { User } from '@generated/type-graphql'
 
@@ -9,5 +10,9 @@ export interface DashboardProps {
 }
 
 export const Dashboard: FC<DashboardProps> = ({ user }) => {
+  const acters = flattenFollowing(user.Acter)
+  if (acters.length === 0) {
+    return <DefaultMessage />
+  }
   return <ActerListByType acters={flattenFollowing(user.Acter)} />
 }
