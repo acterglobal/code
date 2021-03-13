5b07719fd069be8e100a9dff345b021d07dbc4cc
Damon <damon@vestervand.net>
2021-03-13 13:51:14 +0100

    fix imports

--- src/pages/[acterType]/new.tsx
+++ src/pages/[acterType]/new.tsx
@@ -21,9 +21,9 @@ import { composeProps, ComposedGetServerSideProps } from 'lib/compose-props'
 
 import { Acter, ActerType, InterestType, User } from '@generated/type-graphql'
 
-import MUTATE_ACTER_CREATE from 'graphql/mutations/mutate-create-acter.graphql'
-import UPDATE_ACTER from 'graphql/mutations/acter-update.graphql'
-import CREATE_ACTIVITY from 'graphql/mutations/activity-create.graphql'
+import MUTATE_ACTER_CREATE from 'api/mutations/mutate-create-acter.graphql'
+import UPDATE_ACTER from 'api/mutations/acter-update.graphql'
+import CREATE_ACTIVITY from 'api/mutations/activity-create.graphql'
 import { ACTIVITY } from 'src/constants'
 
 /**
