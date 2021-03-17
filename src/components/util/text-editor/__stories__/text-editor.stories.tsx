e3f4221881ccd0167fd4a321a8939db824f7c22e
Malik <malik43213@gmail.com>
2021-03-17 10:48:04 +0100

    fixed issue

--- src/components/util/text-editor/__stories__/text-editor.stories.tsx
+++ src/components/util/text-editor/__stories__/text-editor.stories.tsx
@@ -1,10 +1,21 @@
 import { Story, Meta } from '@storybook/react'
 
-import { TextEditor as TextEditorComponent } from 'src/components/util/text-editor'
+import {
+  TextEditor as TextEditorComponent,
+  TextEditorProps,
+} from 'src/components/util/text-editor'
 
 export default {
   title: 'util/TextEditor',
   component: TextEditorComponent,
+  args: {
+    initialValue: null,
+    width: 400,
+    height: 300,
+    handleInputChange: (data) => null,
+  },
 } as Meta
 
-export const TextEditor: Story = () => <TextEditorComponent />
+export const TextEditor: Story<TextEditorProps> = (args) => (
+  <TextEditorComponent {...args} />
+)
