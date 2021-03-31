fa43a247fc715ca61077f85df0a2892af98ffdfe
Malik <44589493+malik-shaik@users.noreply.github.com>
2021-03-31 11:53:42 +0200

    fixed issue (#134)

--- src/lib/get-camel-case/index.tsx
+++
@@ -1,7 +0,0 @@
-/**
- * Turn a name into initials
- * @param string to be turned into initials
- * @returns a string with the first letter uppercase
- */
-export const getCamelCase = (string: string): string =>
-  string.charAt(0).toUpperCase() + string.slice(1)
