a65e3c73fe887af6c9332b2658742679f8227d1c
Malik <malik43213@gmail.com>
2021-04-13 08:18:08 +0200

    added activity type and story

---
+++ src/components/activity/__stories__/activity-tile.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, Story } from '@storybook/react'
+import {
+  ActivityTile,
+  ActivityTileProps,
+} from 'src/components/activity/new-tile'
+import { ExampleActivity } from 'src/__fixtures__/activity/example-activity'
+
+export default {
+  title: 'landingpage/ActivityTile',
+  component: ActivityTile,
+  args: {
+    activity: ExampleActivity,
+  },
+} as Meta
+
+export const Tile: Story<ActivityTileProps> = (args) => (
+  <ActivityTile {...args} />
+)
