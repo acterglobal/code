aab44f9e27df86e41d9f7a1876ba28ce30faab32
Malik <malik43213@gmail.com>
2021-04-19 19:10:59 +0200

    created search components for activities

---
+++ src/components/activity/__stories__/search-activities.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, Story } from '@storybook/react'
+import { SearchActivities } from 'src/components/activity/search-activities'
+
+export default {
+  title: 'Search/Activities',
+  component: SearchActivities,
+} as Meta
+
+export const Activities: Story = () => <SearchActivities />
