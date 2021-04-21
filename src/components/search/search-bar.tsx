15b1eaa03153ea55f1296e064f7248254fc7892d
Malik <malik43213@gmail.com>
2021-04-21 10:33:36 +0200

    refactored

--- src/components/search/search-bar.tsx
+++ src/components/search/search-bar.tsx
@@ -62,7 +62,7 @@ export const SearchBar: FC = () => {
     groupedOptions,
   } = useAutocomplete({
     id: 'use-autocomplete-demo',
-    options: films,
+    options: data,
     getOptionLabel: (option) => option.title,
   })
   return (
@@ -88,25 +88,4 @@ export const SearchBar: FC = () => {
   )
 }
 
-const films = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: 'Pulp Fiction', year: 1994 },
-  { title: 'The Lord of the Rings: The Return of the King', year: 2003 },
-  { title: 'The Good, the Bad and the Ugly', year: 1966 },
-  { title: 'Fight Club', year: 1999 },
-  { title: 'The Lord of the Rings: The Fellowship of the Ring', year: 2001 },
-  { title: 'Star Wars: Episode V - The Empire Strikes Back', year: 1980 },
-  { title: 'Forrest Gump', year: 1994 },
-  { title: 'Inception', year: 2010 },
-  { title: 'The Lord of the Rings: The Two Towers', year: 2002 },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: 'Goodfellas', year: 1990 },
-  { title: 'The Matrix', year: 1999 },
-  { title: 'Seven Samurai', year: 1954 },
-  { title: 'Star Wars: Episode IV - A New Hope', year: 1977 },
-]
+const data = []
