d9a7c5355b0111a73648fcc721fb82e8608bb0ee
Malik <malik43213@gmail.com>
2021-04-23 15:42:46 +0200

    refactored

--- src/components/acter/__stories__/acter-tile.stories.tsx
+++ src/components/acter/__stories__/acter-tile.stories.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Meta, Story } from '@storybook/react'
-import {
-  ActerTile as ActerTileComponent,
-  ActerTileProps,
-} from 'src/components/acter/tile'
+import { ActerTile, ActerTileProps } from 'src/components/acter/tile'
 import { ExampleActer } from 'src/__fixtures__'
+import { Acter } from '@schema'
 
 export default {
   title: 'search/ActerTile',
-  component: ActerTileComponent,
+  component: ActerTile,
   args: {
     acter: ExampleActer,
   },
 } as Meta
 
-export const ActerTile: Story<ActerTileProps> = (args) => (
-  <ActerTileComponent {...args} />
-)
+const Template: Story<ActerTileProps> = (args) => <ActerTile {...args} />
+
+export const Tile = Template.bind({})
+Tile.args = {
+  acter: {
+    ...ExampleActer,
+  } as Acter,
+}
