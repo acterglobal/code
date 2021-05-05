86280da5a8a7a4598c61656a93e269c99835986c
Damon <damon@uber.com>
2021-05-05 19:54:28 +0200

    Fix activity type table add migration (#159)

--- prisma/migrations/20210426092238_add_activity_type/migration.sql
+++ prisma/migrations/20210426092238_add_activity_type/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - Added the required column `activityTypeId` to the `activities` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "activities" ADD COLUMN     "activityTypeId" TEXT NOT NULL;
-
 -- CreateTable
 CREATE TABLE "activity_types" (
     "id" TEXT NOT NULL,
@@ -16,6 +7,12 @@ CREATE TABLE "activity_types" (
 );
 
 CREATE UNIQUE INDEX "activity_types.name_unique" ON "activity_types"("name");
+
+-- We need this to set the foreign key. The seed process should remove this
+INSERT INTO "activity_types"("id", "name") VALUES ('dummy', 'dummy');
+
+-- AlterTable
+ALTER TABLE "activities" ADD COLUMN "activityTypeId" TEXT NOT NULL DEFAULT 'dummy';
 
 -- AddForeignKey
 ALTER TABLE "activities" ADD FOREIGN KEY ("activityTypeId") REFERENCES "activity_types"("id") ON DELETE CASCADE ON UPDATE CASCADE;
