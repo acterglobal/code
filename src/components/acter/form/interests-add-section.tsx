a8b2c9d5e1368f68fa848635d784dacafae5a73e
Malik <malik43213@gmail.com>
2021-05-08 13:09:35 +0200

    made interest picker

--- src/components/acter/form/interests-add-section.tsx
+++ src/components/acter/form/interests-add-section.tsx
@@ -1,129 +1,30 @@
-import React, { FC, useState, useEffect, ChangeEvent } from 'react'
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
-import { Tabs, Tab, Box } from '@material-ui/core'
-import { InterestTypes } from 'src/components/interests/interest-types'
-import { getTopLevelTypes } from 'src/lib/interests/get-toplevel-types'
-import { interestTypeMap } from 'src/lib/interests/mapInterestTypes'
-import { FormSetFieldValue, FormValues } from 'src/components/acter/form'
+import React, { FC, useState, useEffect } from 'react'
+import { InterestsPicker } from 'src/components/interests/interests-picker'
+import { FormSetFieldValue } from 'src/components/acter/form'
 import { InterestType } from '@schema'
 
 export interface InterestsAddSectionProps {
   interestTypes: InterestType[]
-  initialValues?: FormValues
+  initialValues: string[]
   setFieldValue: FormSetFieldValue
-  showDivider?: boolean
 }
 
-export const InterestsAddSection: FC<InterestsAddSectionProps> = (props) => {
-  const {
-    interestTypes,
-    initialValues = [],
-    setFieldValue,
-    showDivider = true,
-  } = props
-  const classes = useStyles()
-  const [value, setValue] = useState(0)
+export const InterestsAddSection: FC<InterestsAddSectionProps> = ({
+  interestTypes,
+  setFieldValue,
+  initialValues,
+}) => {
   const [selectedInterests, setSelectedInterests] = useState(initialValues)
-  const [selectedTypes, setSelectedTypes] = useState([])
-
-  const topLevelTypes = getTopLevelTypes(interestTypes)
-
-  const getSelectedTypes = (): string[] => {
-    const types = selectedInterests.map(
-      (interestId) => interestTypeMap(interestTypes)[interestId]
-    )
-    return types
-  }
-
-  useEffect(() => {
-    setSelectedTypes(getSelectedTypes())
-  }, [])
 
   useEffect(() => {
     setFieldValue('interestIds', selectedInterests)
   }, [selectedInterests])
 
-  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-  const handleChange = (event: ChangeEvent<any>, newValue: number) => {
-    setValue(newValue)
-  }
-
-  const handleSelectedInterest = (interest, type) => {
-    const newSelectedTypes = [...selectedTypes]
-    if (selectedInterests.includes(interest)) {
-      setSelectedInterests(
-        selectedInterests.filter(
-          (selectedInterest) => selectedInterest !== interest
-        )
-      )
-      const deleteIndex = newSelectedTypes.indexOf(
-        newSelectedTypes.find((newtype) => newtype === type)
-      )
-      newSelectedTypes.splice(deleteIndex, 1)
-      setSelectedTypes([...newSelectedTypes])
-    } else {
-      setSelectedInterests([...selectedInterests, interest])
-      setSelectedTypes([...selectedTypes, type])
-    }
-  }
-
   return (
-    <div className={classes.root}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        indicatorColor="primary"
-        textColor="primary"
-        variant="fullWidth"
-        aria-label="full width tabs example"
-        className={classes.tabs}
-      >
-        {topLevelTypes.map((type) => (
-          <Tab className={classes.tab} label={type.name} key={type.id} />
-        ))}
-      </Tabs>
-
-      <Box>
-        {topLevelTypes.map((type, index) => (
-          <Box
-            className={classes.interests}
-            role="tabpanel"
-            hidden={value !== index}
-            key={index}
-          >
-            {value === index && (
-              <Box>
-                <InterestTypes
-                  type={type}
-                  allTypes={interestTypes}
-                  onSelectedInterestsChange={handleSelectedInterest}
-                  selectedInterests={selectedInterests}
-                  selectedTypes={selectedTypes}
-                  columns={true}
-                  divider={showDivider}
-                />
-              </Box>
-            )}
-          </Box>
-        ))}
-      </Box>
-    </div>
+    <InterestsPicker
+      interestTypes={interestTypes}
+      selectedInterests={selectedInterests}
+      setSelectedInterests={setSelectedInterests}
+    />
   )
 }
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      // width: 600,
-    },
-    tabs: {
-      marginBottom: theme.spacing(1),
-    },
-    tab: {
-      fontWeight: theme.typography.fontWeightBold,
-    },
-    interests: {
-      overflowY: 'scroll',
-    },
-  })
-)
