403d78ad85015177e31b44dad7d84a4fdd933421
Malik <malik43213@gmail.com>
2021-05-11 14:17:58 +0200

    added tests to interest libs

---
+++ src/lib/interests/__tests__/get-selected-interests.test.ts
@@ -0,0 +1,28 @@
+import { getSelectedInterests } from 'src/lib/interests/get-selected-interests'
+import { Interests } from 'src/__fixtures__/interest/interests'
+
+const allInterests = Interests.data.interestTypes
+const selectedInterests = [
+  allInterests[1].Interests[0],
+  allInterests[2].Interests[0],
+]
+
+describe('getSelectedInterests', () => {
+  it('should return empty array if no selected interests provided', () => {
+    expect(getSelectedInterests(allInterests, [])).toStrictEqual([])
+  })
+
+  it('should return selected interests grouped by interestType', () => {
+    const result = getSelectedInterests(allInterests, selectedInterests)
+
+    expect(result[1].Interests).toHaveLength(1)
+    expect(result[1].name).toBe('Environment')
+    expect(result[1].Interests[0].name).toBe('Air')
+
+    expect(result[2].Interests).toHaveLength(1)
+    expect(result[2].name).toBe('Focus')
+    expect(result[2].Interests[0].name).toBe('Partnerships')
+
+    expect(result[3].Interests).toHaveLength(0)
+  })
+})
