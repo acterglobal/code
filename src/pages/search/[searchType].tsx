a6b2896e90ed7632fd67bd9d72f140c362a2894b
Malik <malik43213@gmail.com>
2021-05-12 10:35:58 +0200

    created dynamic search route

---
+++ src/pages/search/[searchType].tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { NextPage } from 'next'
+import { Head } from 'src/components/layout/head'
+import { Layout } from 'src/components/layout'
+import { Search } from 'src/components/search'
+import { composeProps, ComposedGetServerSideProps } from 'lib/compose-props'
+import { getUserProfile, searchActers, getInterests } from 'src/props'
+import { Acter, InterestType, User } from '@schema'
+import { ACTERS, ACTIVITIES } from 'src/constants'
+
+interface SearchPageProps {
+  acters: Acter[]
+  user?: User
+  interestTypes: InterestType[]
+}
+
+const SearchPage: NextPage<SearchPageProps> = ({
+  acters,
+  interestTypes,
+  user,
+}) => (
+  <Layout user={user}>
+    <Head title="Acter" />
+
+    <main>
+      <Search
+        acters={acters}
+        searchType={ACTIVITIES}
+        interestTypes={interestTypes}
+      />
+    </main>
+  </Layout>
+)
+
+export const getServerSideProps: ComposedGetServerSideProps = (ctx) =>
+  composeProps(ctx, getUserProfile(false), searchActers, getInterests)
+
+export default SearchPage
