43271d924f3739687665becb0e064e5fccac13d8
Malik <malik43213@gmail.com>
2021-05-17 16:45:32 +0200

    updated Post migration and removed old ones

--- src/__fixtures__/post/example-post.ts
+++ src/__fixtures__/post/example-post.ts
@@ -4,7 +4,6 @@ import { ExampleUserActer, ExampleOrganisationActer } from 'src/__fixtures__'
 
 export const ExamplePost: Post = {
   id: '7757714c-f537-466c-86df-fdc5b74b56ee',
-  subject: 'Example Post Title',
   content: `This is post content and it is long because 
             Lorem ipsum dolor sit amet consectetur adipisicing
             elit. Quam laudantium quas voluptates assumenda deserunt, sequi
@@ -21,7 +20,6 @@ export const ExamplePost: Post = {
 
 export const ExampleSubPost: Post = {
   id: 'd81402b8-10d1-4bf3-b37a-1261e3a7230a',
-  subject: 'Example Sub Post',
   content: `This is sub message content and it is long because 
   Lorem ipsum dolor sit amet consectetur adipisicing
   elit. Quam laudantium quas voluptates assumenda deserunt, sequi
