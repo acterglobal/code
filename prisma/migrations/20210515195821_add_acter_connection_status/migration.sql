554e24744d112d6f09af878270bdaabe16a8a16e
Damon <damon@vestervand.net>
2021-05-18 14:45:15 +0200

    Set creator as initial Admin

--- prisma/migrations/20210515195821_add_acter_connection_status/migration.sql
+++ prisma/migrations/20210515195821_add_acter_connection_status/migration.sql
@@ -3,3 +3,14 @@ CREATE TYPE "ActerConnectionStatus" AS ENUM ('PENDING', 'MEMBER', 'ADMIN');
 
 -- AlterTable
 ALTER TABLE "acter_connections" ADD COLUMN     "status" "ActerConnectionStatus" NOT NULL DEFAULT E'MEMBER';
+
+-- Set creating UserActer as initial admin
+UPDATE "acter_connections" SET "status"='ADMIN' WHERE "id" IN (
+  SELECT 
+    "ac"."id"
+  FROM "acter_connections" AS "ac"
+  JOIN "acters" AS "follower" ON "ac"."follower_acter_id" = "follower"."id"
+  JOIN "users" ON "users"."id" = "follower"."created_by_user_id"
+  JOIN "acters" AS "following" ON "ac"."following_acter_id" = "following"."id"
+  WHERE "users"."id" = "following"."created_by_user_id"
+)
