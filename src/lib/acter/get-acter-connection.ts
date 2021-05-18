fb0e360fee51ea8530907af79be5fa2fa5d18dec
Damon <damon@vestervand.net>
2021-05-18 14:44:32 +0200

    Add getActerConnection lib function

---
+++ src/lib/acter/get-acter-connection.ts
@@ -0,0 +1,20 @@
+import { Acter, ActerConnection } from '@schema'
+
+/**
+ * Function to look for an ActerConnection on `acter` for a given `follower`
+ * @param acter Acter for which we are looking at existing connections
+ * @param potentialFollower Follwer Acter which may or may not be already connected to Acter
+ * @returns An ActerConnection if one exists, or null if none
+ */
+export const getActerConnection = (
+  acter: Acter,
+  potentialFollower: Acter
+): ActerConnection | undefined => {
+  if (!acter.Followers?.length) {
+    return
+  }
+
+  return acter.Followers.find(
+    ({ Follower }) => Follower.id === potentialFollower.id
+  )
+}
