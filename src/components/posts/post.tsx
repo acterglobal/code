8b936f58b97cc152038fb19d40daa4c1e9ee5957
jmusyoki <jmusyoki@wastehero.io>
2021-05-21 10:10:19 +0200

    fixed avatar component error

--- src/components/posts/post.tsx
+++ src/components/posts/post.tsx
@@ -13,6 +13,8 @@ export interface PostsProps {
 export const Posts: FC<PostsProps> = ({ post, comment }) => {
   const classes = useStyles()
 
+  console.log('Single Post',post.Author)
+
   return (
     <>
       <ActerAvatar acter={post.Author} size={6} />
