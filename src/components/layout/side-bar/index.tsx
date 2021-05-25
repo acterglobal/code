9c5c6f7bb75c2bb68f4a33525c5156418f2287b9
Malik <malik43213@gmail.com>
2021-05-25 14:58:32 +0200

    WIP

--- src/components/layout/side-bar/index.tsx
+++ src/components/layout/side-bar/index.tsx
@@ -89,10 +89,7 @@ const IconMenuItem: FC<IconMenuItemProps> = ({ Icon, href, text }) => {
     <ListItem className={classes.item}>
       <Link href={href}>
         <a>
-          <Icon
-            fontSize={text === 'Acter' ? 'large' : 'default'}
-            aria-label={text}
-          />
+          <Icon fontSize="large" aria-label={text} />
         </a>
       </Link>
     </ListItem>
