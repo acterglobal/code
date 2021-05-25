b23c8883d27111beeb68eac34da09ccfb25c595d
Malik <malik43213@gmail.com>
2021-05-25 15:30:12 +0200

    changed migration script

--- prisma/migrations/20210525112200_change_user_join_setting_to_acter_join_setting/migration.sql
+++ prisma/migrations/20210525112200_change_user_join_setting_to_acter_join_setting/migration.sql
@@ -1,9 +1,3 @@
-/*
-  Warnings:
 
-  - You are about to drop the column `user_join_setting` on the `acters` table. All the data in the column will be lost.
-
-*/
 -- AlterTable
-ALTER TABLE "acters" DROP COLUMN "user_join_setting",
-ADD COLUMN     "acter_join_setting" "ActerJoinSettings" NOT NULL DEFAULT E'EVERYONE';
+ALTER TABLE "acters" RENAME COLUMN "user_join_setting" TO "acter_join_setting"
