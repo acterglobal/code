f21335842699aa14e3e493c5b88eda4431791a5f
Damon <damon@vestervand.net>
2021-05-31 14:36:36 +0200

    Add wrapper function to followerHasRoleOnActer specific to User use case

---
+++ src/lib/user/user-has-role-on-acter.ts
@@ -0,0 +1,20 @@
+import { followerHasRoleOnActer } from 'src/lib/acter/follower-has-role-on-acter'
+import { Acter, ActerConnectionRole, User } from '@schema'
+
+/**
+ * Wrapper for followerHasRoleOnActer specific to users which handles the case when a user is not logged in
+ * @param user User for which we are checking roles
+ * @param role Minimum role required
+ * @param acter Acter on which we are checking roles
+ * @returns true if follower has roles, false if not
+ */
+export const userHasRoleOnActer = (
+  user: User | undefined,
+  role: ActerConnectionRole,
+  acter: Acter
+): boolean => {
+  if (!user?.Acter) {
+    return false
+  }
+  return followerHasRoleOnActer(user.Acter, role, acter)
+}
