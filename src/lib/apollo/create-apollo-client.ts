b4a449a1424a95d64d6e8f2518674731f49c8688
Damon <damon@vestervand.net>
2021-06-01 15:40:59 +0200

    Add enhanced Apollo client error display

--- src/lib/apollo/create-apollo-client.ts
+++ src/lib/apollo/create-apollo-client.ts
@@ -1,21 +1,33 @@
 import {
   ApolloClient,
   HttpLink,
+  from,
   InMemoryCache,
   NormalizedCacheObject,
 } from '@apollo/client'
+
+import { onError } from '@apollo/client/link/error'
 
 export const createApolloClient = (
   uri: string
 ): ApolloClient<InMemoryCache | NormalizedCacheObject> => {
   const ssrMode = typeof window === 'undefined'
-  const link = new HttpLink({
+  const httpLink = new HttpLink({
     uri,
+  })
+
+  const errorLink = onError(({ networkError }) => {
+    //@ts-ignore
+    if (networkError.result?.errors) {
+      console.error('Apollo GraphQL validation errors:')
+      //@ts-ignore
+      console.error(networkError.result.errors)
+    }
   })
 
   return new ApolloClient({
     cache: new InMemoryCache(),
     ssrMode,
-    link,
+    link: from([errorLink, httpLink]),
   })
 }
