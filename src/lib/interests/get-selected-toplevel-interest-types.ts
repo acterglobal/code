c8b1c8922d32b6ffb29b82e30e15285d41af735e
Jude Musyoki <judemusyoki@gmail.com>
2021-06-08 11:19:29 +0200

    fixed getSelectedTopLevelTypes error

--- src/lib/interests/get-selected-toplevel-interest-types.ts
+++ src/lib/interests/get-selected-toplevel-interest-types.ts
@@ -1,14 +1,13 @@
 import { Interest, InterestType } from '@schema'
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const getSelectedTopLevelTypes = (
   topLevelTypes: InterestType[],
   selected: Interest[]
-) => {
+): InterestType[] => {
   const selectedTopLevel = []
   topLevelTypes.map((type) => {
     selected.map((select) => {
-      type.name === select.InterestType.name ||
+      type.id === select.InterestType.id ||
       type.id === select.InterestType.parentInterestTypeId
         ? selectedTopLevel.includes(type)
           ? null
