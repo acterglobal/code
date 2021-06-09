3806c4f966abd3f5372e62fba466cb61ab2efc1a
Jude Musyoki <judemusyoki@gmail.com>
2021-06-10 00:32:02 +0200

    updated link fixture

--- src/components/links/__stories__/links.stories.tsx
+++ src/components/links/__stories__/links.stories.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Meta, Story } from '@storybook/react'
 import { Links } from 'src/components/links'
+import { ExampleLink, ExampleActer, ExampleUser } from 'src/__fixtures__'
 
 export default {
   title: 'Acter/Link',
   component: Links,
+  args: {
+    links: [
+      {
+        ...ExampleLink,
+      },
+    ],
+    user: { ...ExampleUser },
+    acter: { ...ExampleActer },
+  },
 } as Meta
 
-export const LinkSection: Story = () => <Links />
+export const LinkSection: Story = (args) => <Links {...args} />
