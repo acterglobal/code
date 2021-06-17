c180e5520a9ef99b5bfca25f054b3b15020cfc3c
Jude Musyoki <judemusyoki@gmail.com>
2021-06-17 14:38:58 +0200

    fixed active search tabs

--- src/components/layout/side-bar/search-menu/tabs.tsx
+++ src/components/layout/side-bar/search-menu/tabs.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react'
 import { Box, createStyles, makeStyles, Theme } from '@material-ui/core'
 import clsx from 'clsx'
 import { SearchType } from 'src/constants'
-import { menuActiveTabColor } from 'src/themes/colors'
+import { menuActiveTabColor, subMenuBackgroundColor } from 'src/themes/colors'
 import Link from 'next/link'
 
 export interface SearchTabsProps {
@@ -12,7 +12,7 @@ export interface SearchTabsProps {
 export const SearchTabs: FC<SearchTabsProps> = ({ activeTab }) => {
   const classes = useStyles()
   const { ACTERS, ACTIVITIES } = SearchType
-  const searchTypes = [ACTIVITIES, ACTERS]
+  const searchTypes = [ACTERS, ACTIVITIES]
 
   return (
     <Box className={classes.tabs}>
@@ -44,16 +44,18 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     tab: {
       width: '100%',
+      height: '100%',
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
       cursor: 'pointer',
       fontSize: theme.spacing(1.5),
       textTransform: 'capitalize',
+      backgroundColor: menuActiveTabColor,
     },
     activeTab: {
       height: '100%',
-      backgroundColor: menuActiveTabColor,
+      backgroundColor: subMenuBackgroundColor,
       color: 'white',
       fontWeight: theme.typography.fontWeightBold,
     },
