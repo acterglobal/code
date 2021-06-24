a2868eeb600985874fa63091adb56a20022f4482
Damon <damon@uber.com>
2021-06-24 12:18:44 +0200

    Remove old next-auth tables (#247)

---
+++ prisma/migrations/20210624082644_remove_unused_next_auth_tables/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the `accounts` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `sessions` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `verification_requests` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "accounts";
+
+-- DropTable
+DROP TABLE "sessions";
+
+-- DropTable
+DROP TABLE "verification_requests";
