a9a95f2718e75393b0083bd577941bf977d56241
Damon <damon@vestervand.net>
2021-06-24 16:23:54 +0200

    Migrate lib to Yarn workspace

---
+++ src/components/posts/index.tsx
@@ -0,0 +1,93 @@
+import React, { FC } from 'react'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
+import { grey } from '@material-ui/core/colors'
+import { Box, Divider } from '@material-ui/core'
+import { Post } from 'src/components/posts/post'
+import { PostForm, PostFormProps } from 'src/components/posts/form'
+import { userHasRoleOnActer } from '@acter/lib/user/user-has-role-on-acter'
+import { Acter, ActerConnectionRole, Post as PostType, User } from '@schema'
+
+export interface PostListProps extends PostFormProps {
+  /**
+   * Acter on which we are viewing posts
+   */
+  acter: Acter
+  /**
+   * User viewing posts
+   */
+  user: User
+  /**
+   * Posts to display
+   */
+  posts: PostType[]
+}
+
+export const PostList: FC<PostListProps> = ({
+  acter,
+  user,
+  posts,
+  onPostSubmit,
+}) => {
+  const classes = useStyles()
+
+  const isUserActerFollower = acter.Followers.find(
+    ({ Follower }) => Follower.id === user?.Acter.id
+  )
+
+  if (!user || !isUserActerFollower) {
+    return null
+  }
+
+  return (
+    <Box className={classes.mainContainer}>
+      {userHasRoleOnActer(user, ActerConnectionRole.MEMBER, acter) && (
+        <PostForm user={user} onPostSubmit={onPostSubmit} />
+      )}
+      {posts?.map((post) => (
+        <Box key={post.id} className={classes.contentContainer}>
+          <Post post={post} />
+          <Divider variant="middle" className={classes.divider} />
+          {post.Comments?.map((comment) => (
+            <Box key={comment.id} className={classes.contentContainer}>
+              <Post key={comment.id} post={comment} commenting />
+            </Box>
+          ))}
+          <Box>
+            <PostForm post={post} user={user} onPostSubmit={onPostSubmit} />
+          </Box>
+        </Box>
+      ))}
+    </Box>
+  )
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    mainContainer: {
+      borderRadius: 7,
+      width: '100%',
+      display: 'flex',
+      flexWrap: 'wrap',
+      marginBottom: theme.spacing(1),
+      [theme.breakpoints.down('xs')]: {
+        width: 300,
+      },
+    },
+    contentContainer: {
+      backgroundColor: 'white',
+      borderRadius: 7,
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      padding: theme.spacing(1),
+      [theme.breakpoints.down('xs')]: {
+        width: 300,
+      },
+      marginTop: theme.spacing(1),
+    },
+    divider: {
+      backgroundColor: grey[500],
+      marginTop: 8,
+    },
+  })
+)
