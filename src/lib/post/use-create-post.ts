857170c2ba99128f40e470cf4b8127d2eaf31133
Jude Musyoki <judemusyoki@gmail.com>
2021-06-30 13:13:33 +0200

    added type descriptions to hook

--- src/lib/post/use-create-post.ts
+++ src/lib/post/use-create-post.ts
@@ -1,30 +1,45 @@
 import { useState } from 'react'
 import { MutationResult } from '@apollo/client'
-import { useNotificationMutation } from 'src/lib/apollo/use-notification-mutation'
+import {
+  UseMutationOptions,
+  useNotificationMutation,
+} from 'src/lib/apollo/use-notification-mutation'
 import CREATE_POST from 'api/mutations/post-create.graphql'
 import CREATE_COMMENT from 'api/mutations/comment-create.graphql'
 import GET_POSTS from 'api/queries/posts-by-acter.graphql'
-import { Post, Acter, User } from '@schema'
+import { Post as PostType, Acter, User } from '@schema'
 
-export type HandleMethod = (values: unknown) => Promise<void>
+export type PostVariables = {
+  acterId: string
+  authorId: string
+}
+
+type CreatePostData = {
+  createPost: PostType
+}
+interface CreatePostOptions
+  extends UseMutationOptions<CreatePostData, PostVariables> {
+  onCompleted: (DeletePostData) => PostType[] | void
+}
+
+export type HandleMethod = (post: PostType | TData) => Promise<void>
 
 /**
  * Custom hook that creates new post
- * @param post
+ * @params acter user displayPostList
  * @returns handle method to create post
  * @returns mutation results from apollo
  */
 export const useCreatePost = (
   acter: Acter,
   user: User,
-  displayPostList: Post[],
-  onComplete: (postList: Post[]) => void
-): [HandleMethod, MutationResult] => {
+  displayPostList: PostType[]
+): [HandleMethod<CreatePostOptions>, MutationResult] => {
   const [isComment, setIsComment] = useState(false)
 
   const createNewPostList = (
     newPost: { parentId: string },
-    displayPostList: any[]
+    displayPostList: PostType[]
   ) => {
     if (newPost.parentId !== null) {
       const newPostList = displayPostList.map((post) => {
@@ -43,26 +58,38 @@ export const useCreatePost = (
     }
   }
 
-  const [createPost, mutationResult] = useNotificationMutation(
-    isComment ? CREATE_COMMENT : CREATE_POST,
-    {
-      update: (cache, { data }) => {
-        const { createPost: newPost } = data
+  const [createPost, mutationResult] = useNotificationMutation<
+    CreatePostData,
+    PostVariables
+  >(isComment ? CREATE_COMMENT : CREATE_POST, {
+    ...options,
+    update: (cache, result) => {
+      typeof options?.update === 'function' && options.update(cache, result)
+      const {
+        data: { createPost: newPost },
+      } = result
 
-        const newPostList = createNewPostList(newPost, displayPostList)
+      const newPostList = createNewPostList(newPost, displayPostList)
 
-        onComplete(newPostList)
+      cache.writeQuery({
+        query: GET_POSTS,
+        data: {
+          posts: newPostList,
+        },
+      })
+    },
+    onCompleted: (result) => {
+      const { createPost: newPost } = result
 
-        cache.writeQuery({
-          query: GET_POSTS,
-          data: {
-            posts: newPostList,
-          },
-        })
-      },
-      getSuccessMessage: () => (isComment ? 'Comment created' : 'Post created'),
-    }
-  )
+      const newPostList = createNewPostList(newPost, displayPostList)
+
+      typeof options?.onCompleted === 'function' &&
+        options.onCompleted(newPostList)
+
+      return newPostList
+    },
+    getSuccessMessage: () => (isComment ? 'Comment created' : 'Post created'),
+  })
 
   const handlePost = async (values) => {
     setIsComment(values.parentId ? true : false)
