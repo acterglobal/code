19e78d90c64dd1de2b353d9485e650abdfbf3a06
Damon <damon@acter.global>
2021-07-05 10:03:47 +0200

    Refactor profile edit into separate pages using settings layout (#239)
    * Refactor create/updateActer hook with better TS & to not set calling component state.
    
    * Refactor profile edit into separate pages using settings layout
    
    * Smarter profile settings navigation
    
    * Damon/settings radio buttons (#240)
    
    * Smarter profile settings navigation
    
    * Refactor settings button into its own component

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,60 +1,20 @@
-import React from 'react'
-import { NextPage } from 'next'
+import { GetServerSideProps } from 'next'
+import { useRouter } from 'next/router'
 
-import { updateActerWithPictures } from 'src/lib/acter/update-acter-with-pictures'
-import { useNotificationMutation } from 'src/lib/apollo/use-notification-mutation'
+const defaultRoute = '/profile/info'
 
-import { Layout } from 'src/components/layout'
-import { Head } from 'src/components/layout/head'
-import { ProfileEdit } from 'src/components/user/profile-edit'
-
-import { Acter, InterestType, User } from '@schema'
-
-import { composeProps, ComposedGetServerSideProps } from 'lib/compose-props'
-import { getUserProfile, getInterests } from 'src/props'
-
-import UPDATE_ACTER from 'api/mutations/acter-update.graphql'
-
-//TODO: write types for formData
-export const _handleSubmit = (
-  acter: Acter,
-  //eslint-disable-next-line @typescript-eslint/no-explicit-any
-  updateActer: (any) => Promise<any>
-  //eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
-) => (formData: any): Promise<any> =>
-  updateActerWithPictures({ acter, formData, updateActer })
-
-interface UserProfilePageProps {
-  loading?: boolean
-  error?: string
-  interestTypes: InterestType[]
-  user?: User
+export const UserProfilePage = (): void => {
+  const router = useRouter()
+  router.replace(defaultRoute)
 }
 
-export const UserProfilePage: NextPage<UserProfilePageProps> = ({
-  user,
-  interestTypes,
-}) => {
-  const [updateUser, { loading }] = useNotificationMutation(UPDATE_ACTER, {
-    getSuccessMessage: () => 'Profile updated',
-  })
-
-  return (
-    <Layout user={user}>
-      <Head title="Profile - Acter" />
-      <main>
-        <ProfileEdit
-          user={user}
-          interestTypes={interestTypes}
-          onSubmit={_handleSubmit(user.Acter, updateUser)}
-          loading={loading}
-        />
-      </main>
-    </Layout>
-  )
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: defaultRoute,
+      permanent: false,
+    },
+  }
 }
-
-export const getServerSideProps: ComposedGetServerSideProps = (ctx) =>
-  composeProps(ctx, getUserProfile(true), getInterests)
 
 export default UserProfilePage
---
+++ src/pages/profile/info.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { NextPage } from 'next'
+import { useUpdateActer } from 'src/lib/acter/use-update-acter'
+import { Layout } from 'src/components/layout'
+import { Head } from 'src/components/layout/head'
+import { ProfileInfoForm } from 'src/components/user/form/info'
+import { InterestType, User } from '@schema'
+import { composeProps, ComposedGetServerSideProps } from 'lib/compose-props'
+import { getUserProfile } from 'src/props'
+
+interface UserProfilePageProps {
+  interestTypes: InterestType[]
+  user?: User
+}
+
+export const UserProfileInfoPage: NextPage<UserProfilePageProps> = ({
+  user,
+}) => {
+  const [updateActer] = useUpdateActer(user.Acter)
+  return (
+    <Layout user={user}>
+      <Head title="Profile - Acter" />
+      <main>
+        <ProfileInfoForm user={user} onSubmit={updateActer} />
+      </main>
+    </Layout>
+  )
+}
+
+export const getServerSideProps: ComposedGetServerSideProps = (ctx) =>
+  composeProps(ctx, getUserProfile(true))
+
+export default UserProfileInfoPage
