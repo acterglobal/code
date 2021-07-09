e1120341df24fd159ac687033ebfb82822e71c6f
Damon <damon@vestervand.net>
2021-07-09 14:18:07 +0200

    Don't log queries

--- packages/schema/prisma/index.ts
+++ packages/schema/prisma/index.ts
@@ -16,9 +16,7 @@ if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient()
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient({
-      log: ['query'],
-    })
+    global.prisma = new PrismaClient()
   }
 
   prisma = global.prisma
