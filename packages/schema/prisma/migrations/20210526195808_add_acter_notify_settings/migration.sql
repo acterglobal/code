114321e15726a4e363ac1b6ec2b96bd9afbefaa1
Damon <damon@vestervand.net>
2021-07-15 14:27:34 +0200

    WIP email notification frequency

--- packages/schema/prisma/migrations/20210526195808_add_acter_notify_settings/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "ActerNotificationSettings" AS ENUM ('ALL_ACTIVITY', 'MENTIONS', 'NONE');
-
--- AlterTable
-ALTER TABLE "acters" ADD COLUMN     "acter_notify_setting" "ActerNotificationSettings" NOT NULL DEFAULT E'NONE';
