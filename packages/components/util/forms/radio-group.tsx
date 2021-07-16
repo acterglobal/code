b4182399eddf40d69e1fe7fb22be41d34d8885d9
Damon <damon@vestervand.net>
2021-07-16 11:11:41 +0200

    Add more settings

---
+++ packages/components/util/forms/radio-group.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from 'react'
+import { Field } from 'formik'
+import { RadioGroup as FormikRadioGroup } from 'formik-material-ui'
+import {
+  FormControl,
+  FormLabel,
+  createStyles,
+  makeStyles,
+  Theme,
+} from '@material-ui/core'
+
+export interface RadioGroupProps {
+  /**
+   * Label to be used for entire radio group
+   */
+  label: string
+  /**
+   * Name of the field on values
+   */
+  name: string
+}
+
+export const RadioGroup: FC<RadioGroupProps> = ({ label, name, children }) => {
+  const classes = useStyles()
+
+  return (
+    <FormControl component="fieldset" fullWidth className={classes.control}>
+      <FormLabel component="legend">{label}</FormLabel>
+      <Field component={FormikRadioGroup} name={name}>
+        {children}
+      </Field>
+    </FormControl>
+  )
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    control: {
+      marginBottom: theme.spacing(2),
+    },
+  })
+)
