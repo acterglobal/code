ea4a2adf024f7b3dacdfafdb8d95d36c0f987065
Damon <damon@vestervand.net>
2021-07-19 09:12:38 +0200

    Update Notification on Email send

--- packages/jobs/src/index.ts
+++ packages/jobs/src/index.ts
@@ -1,3 +1,6 @@
 import 'reflect-metadata'
-import { emailSendWorker as _emailWorker } from './workers/email-send-worker'
-import { notificationCreateWorker as _notificationWorker } from './workers/notification-create-worker'
+import { emailSendWorker } from './workers/email-send-worker'
+import { notificationCreateWorker } from './workers/notification-create-worker'
+
+emailSendWorker.resume()
+notificationCreateWorker.resume()
