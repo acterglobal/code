31deacc96b4d00f0418fa1cd34463dfd5cb69c53
Damon <damon@vestervand.net>
2021-08-04 14:46:44 +0200

    use babel-jest instead of ts-jest for speed

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  presets: ['@babel/preset-react'],
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
   plugins: [
     'babel-plugin-transform-typescript-metadata',
     ['@babel/plugin-proposal-decorators', { legacy: true }],
