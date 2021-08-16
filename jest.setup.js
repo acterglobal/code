e135192d05d2ed96ab13b0e3ef05011e92a5556b
Damon <damon@acter.global>
2021-08-16 20:16:56 +0200

    Damon/upgrade next 11.1 (#319)
    * Update Next.js to 11.1 for security update
    
    * Update images paths so next/image works again

--- jest.setup.js
+++ jest.setup.js
@@ -1,13 +1,2 @@
 require('reflect-metadata')
 require('@testing-library/jest-dom')
-
-process.env = {
-  ...process.env,
-  __NEXT_IMAGE_OPTS: {
-    deviceSizes: [320, 420, 768, 1024, 1200],
-    imageSizes: [],
-    domains: ['acter-dev.imgix.net'],
-    path: '/_next/image',
-    loader: 'imgix',
-  },
-}
