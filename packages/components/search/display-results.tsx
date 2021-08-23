ac71b4639e6121a83f65cb84ef9d0fea57492ed3
malik <malik.shaik@outlook.com>
2021-08-23 09:34:01 +0200

    added infinite scroll

--- packages/components/search/display-results.tsx
+++ packages/components/search/display-results.tsx
@@ -6,35 +6,52 @@ import { ActivityTile } from '@acter/components/activity/tile'
 import { ActerTile } from '@acter/components/acter/tile'
 import { SearchType } from '@acter/lib/constants'
 import { acterAsUrl } from '@acter/lib/acter/acter-as-url'
+import { useFetchActers } from '@acter/lib/acter/use-fetch-acters'
 import { Link } from '@acter/components/util/anchor-link'
 import clsx from 'clsx'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 const { ACTIVITIES, ACTERS } = SearchType
 export interface DisplayResultsProps {
-  searchType: string
-  acters: Acter[]
+  searchType: SearchType
   interestTypes: InterestType[]
 }
 
-export const DisplayResults: FC<DisplayResultsProps> = (props) => {
-  const { searchType, acters, interestTypes } = props
+export const DisplayResults: FC<DisplayResultsProps> = ({
+  searchType,
+  interestTypes,
+}) => {
   const classes = useStyles()
+  const [data, { loading, loadMore }] = useFetchActers(searchType)
+  const { acters } = data
 
   return (
     <Box className={clsx(classes.root, classes[searchType])}>
       {acters.length !== 0 ? (
-        acters.map((acter, index) => (
-          <Box className={classes.singleItem} key={index} role="listitem">
-            <Link href={acterAsUrl({ acter })} passHref>
-              {searchType === ACTERS && (
-                <ActerTile acter={acter} interestTypes={interestTypes} />
-              )}
-              {searchType === ACTIVITIES && (
-                <ActivityTile activity={acter.Activity} />
-              )}
-            </Link>
-          </Box>
-        ))
+        <InfiniteScroll
+          dataLength={acters.length}
+          next={loadMore}
+          hasMore={true}
+          loader={<span>Loading...</span>}
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          {acters.map((acter, index) => (
+            <Box className={classes.singleItem} key={index} role="listitem">
+              <Link href={acterAsUrl({ acter })} passHref>
+                {searchType === ACTERS && (
+                  <ActerTile acter={acter} interestTypes={interestTypes} />
+                )}
+                {searchType === ACTIVITIES && (
+                  <ActivityTile activity={acter.Activity} />
+                )}
+              </Link>
+            </Box>
+          ))}
+        </InfiniteScroll>
       ) : (
         <Typography variant="body2" aria-label="zero-acters">
           Your search did not return any results. Try removing search terms
