6e70f70870cb23f98d1d996f0ecd80f6dadc66e0
Damon <damon@vestervand.net>
2021-08-29 14:56:57 +0200

    Add usePaginatedQuery hook for generic, cursor-based pagination

---
+++ packages/lib/apollo/__tests__/use-paginated-query.test.ts
@@ -0,0 +1,141 @@
+import { renderHook } from '@testing-library/react-hooks'
+import { useQuery } from '@apollo/client'
+import {
+  usePaginatedQuery,
+  _getOnCompleted,
+  Pagination,
+  UsePaginatedResults,
+} from '@acter/lib/apollo/use-paginated-query'
+
+jest.mock('@apollo/client')
+
+describe('usePaginatedQuery', () => {
+  const useQueryMock = useQuery as jest.Mock
+
+  it('should send the first request with no cursor', () => {
+    useQueryMock.mockImplementation((_query, { variables }) => {
+      expect(variables).toEqual({
+        skip: 0,
+        take: 10,
+        cursor: null,
+      })
+    })
+
+    renderHook(() => usePaginatedQuery('', '', { variables: {} }))
+  })
+
+  describe('onComplete', () => {
+    const paginationDefaults: Pagination = {
+      cursor: null,
+      skip: 0,
+      take: 10,
+    }
+    const pagination = paginationDefaults
+    const resultKey = 'foo'
+    const results = []
+    const setPagination = jest.fn()
+    const setHasMore = jest.fn()
+    const setResults = jest.fn()
+    const getOnCompletedParams = {
+      pagination,
+      paginationDefaults,
+      resultKey,
+      results,
+      setPagination,
+      setHasMore,
+      setResults,
+    }
+    const data = {
+      foo: [...Array(10)].map((_, id) => ({ id })),
+    }
+
+    beforeEach(() => {
+      //eslint-disable-next-line @typescript-eslint/no-extra-semi
+      ;[setPagination, setHasMore, setResults].forEach((fn) => fn.mockReset())
+    })
+
+    it('should skip pagination if there are no results', () => {
+      _getOnCompleted(getOnCompletedParams)({})
+      expect(setPagination).not.toHaveBeenCalled()
+      expect(setHasMore).not.toHaveBeenCalled()
+      expect(setResults).not.toHaveBeenCalled()
+    })
+
+    it('should skip pagination if the results do not include an id', () => {
+      _getOnCompleted(getOnCompletedParams)({ foo: [{ bar: 'blah' }] })
+      expect(setPagination).not.toHaveBeenCalled()
+      expect(setHasMore).not.toHaveBeenCalled()
+      expect(setResults).not.toHaveBeenCalled()
+    })
+
+    it('should set the cursor to the id of the last result and skip 1 in pagination', () => {
+      _getOnCompleted(getOnCompletedParams)(data)
+      expect(setPagination).toHaveBeenCalledTimes(1)
+      expect(setPagination).toHaveBeenCalledWith({
+        cursor: 9,
+        skip: 1,
+        take: 10,
+      })
+    })
+
+    it('should set hasMore when there appear to be more results', () => {
+      _getOnCompleted(getOnCompletedParams)(data)
+      expect(setHasMore).toHaveBeenCalledTimes(1)
+      expect(setHasMore).toHaveBeenCalledWith(true)
+    })
+
+    it('should set results', () => {
+      _getOnCompleted(getOnCompletedParams)(data)
+      expect(setResults).toHaveBeenCalledTimes(1)
+    })
+
+    it('should call onCompleted from options if it exists', () => {
+      const onCompleted = jest.fn()
+      _getOnCompleted({ ...getOnCompletedParams, onCompleted })(data)
+
+      expect(onCompleted).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('refetch', () => {
+    it('should refetch wtih the same pagination & variables by default', () => {
+      const pagination = { cursor: 'foo', skip: 1, take: 10 }
+      const variables = { foo: 'bar' }
+      // const refetch = jest.fn()
+
+      useQueryMock.mockReturnValue({
+        refetch: (refetchOptions) => {
+          console.log(refetchOptions)
+          expect(refetchOptions).toEqual({ ...variables, ...pagination })
+        },
+      })
+
+      const {
+        result: { current },
+      } = renderHook<any, UsePaginatedResults>(() =>
+        usePaginatedQuery('', '', { pagination, variables })
+      )
+      current.refetch()
+    })
+
+    it('should refetch with fresh pagination when requested', () => {
+      const pagination = { cursor: 'foo', skip: 1, take: 10 }
+      const variables = { foo: 'bar' }
+      // const refetch = jest.fn()
+
+      useQueryMock.mockReturnValue({
+        refetch: (refetchOptions) => {
+          console.log(refetchOptions)
+          expect(refetchOptions).toEqual({ ...variables, ...pagination })
+        },
+      })
+
+      const {
+        result: { current },
+      } = renderHook<any, UsePaginatedResults>(() =>
+        usePaginatedQuery('', '', { pagination, variables })
+      )
+      current.refetch({ resetPagination: true })
+    })
+  })
+})
