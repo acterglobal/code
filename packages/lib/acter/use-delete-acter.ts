f2e111d908a216f3fc556cdcd4677d9e321fb20f
Malik <44589493+malik-shaik@users.noreply.github.com>
2021-09-06 13:41:09 +0200

    Malik/Implement useDeleteActer hook and fix bug (#349)
    * made useDeleteActer hook
    
    * fixed bug

---
+++ packages/lib/acter/use-delete-acter.ts
@@ -0,0 +1,38 @@
+import { FetchResult, MutationResult } from '@apollo/client'
+import DELETE_ACTER from '@acter/schema/mutations/acter-delete.graphql'
+import { useNotificationMutation } from '@acter/lib/apollo/use-notification-mutation'
+import { acterAsUrl } from '@acter/lib/acter/acter-as-url'
+import { useRouter } from 'next/router'
+
+type HandleMethod = (acterId: string) => Promise<FetchResult>
+
+/**
+ * To delete acter from DB and update cache
+ * @param acterId
+ * @returns delete handle method and mutation results
+ */
+export const useDeleteActer = (): [HandleMethod, MutationResult] => {
+  const router = useRouter()
+  const [deleteActer, { ...restQueryResult }] = useNotificationMutation(
+    DELETE_ACTER,
+    {
+      update: (cache, { data }) =>
+        cache.modify({
+          id: cache.identify(data.deleteActer),
+          fields: (_, { DELETE }) => DELETE,
+        }),
+
+      onCompleted: (data) => {
+        const redirectUrl = data.deleteActer.Parent
+          ? acterAsUrl({ acter: data.deleteActer.Parent })
+          : '/dashboard'
+        router.push(redirectUrl)
+      },
+
+      getSuccessMessage: (data) => `${data.deleteActer.name} is deleted`,
+    }
+  )
+  const handleDeleteActer = (acterId) => deleteActer({ variables: { acterId } })
+
+  return [handleDeleteActer, { ...restQueryResult }]
+}
