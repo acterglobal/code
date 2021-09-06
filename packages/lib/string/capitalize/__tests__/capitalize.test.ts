abb12f7abfac5696f0328cc677b1baec9055d7db
Damon <damon@vestervand.net>
2021-09-06 13:32:59 +0200

    Add test

---
+++ packages/lib/string/capitalize/__tests__/capitalize.test.ts
@@ -0,0 +1,13 @@
+import { capitalize } from '@acter/lib/string/capitalize'
+
+describe('capitalize', () => {
+  it('should return an empty string if a value passed is empty or missing', () => {
+    const undef = undefined
+    expect(capitalize(undef)).toBe('')
+    expect(capitalize('')).toBe('')
+  })
+
+  it('should capitalize a passed string', () => {
+    expect(capitalize('foo bar')).toBe('Foo bar')
+  })
+})
