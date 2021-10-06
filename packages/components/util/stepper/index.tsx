70a6e4e965373cd7c22db91217c696224dbf9b04
Jude Musyoki <judemusyoki@gmail.com>
2021-10-06 20:07:49 +0200

    added links to breadcrumbs

--- packages/components/util/stepper/index.tsx
+++ packages/components/util/stepper/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC, ReactNode } from 'react'
 
-import { Box, createStyles, makeStyles, Theme } from '@material-ui/core'
+import { Box, Button, createStyles, makeStyles, Theme } from '@material-ui/core'
 import { grey } from '@material-ui/core/colors'
 
 import clsx from 'clsx'
@@ -8,17 +8,34 @@ import clsx from 'clsx'
 export interface StepperProps {
   steps: ReactNode[]
   activeStep: number
+  handleSelectStep: (value: number) => void
 }
 
-export const Stepper: FC<StepperProps> = ({ steps, activeStep }) => {
+export const Stepper: FC<StepperProps> = ({
+  steps,
+  activeStep,
+  handleSelectStep,
+}) => {
   const classes = useStyles()
+
+  const handleClick = (selectedStep: number) => {
+    selectedStep <= activeStep && handleSelectStep(selectedStep)
+  }
+
   return (
     <Box className={classes.statusBars}>
       {steps.map((step, index) => (
-        <Box
-          key={index}
-          className={clsx(classes.bar, activeStep >= index && classes.active)}
-        ></Box>
+        <Button
+          variant="text"
+          color="primary"
+          className={classes.button}
+          onClick={() => handleClick(index)}
+        >
+          <Box
+            key={index}
+            className={clsx(classes.bar, activeStep >= index && classes.active)}
+          ></Box>
+        </Button>
       ))}
     </Box>
   )
@@ -36,11 +53,18 @@ const useStyles = makeStyles((theme: Theme) =>
       height: 8,
       backgroundColor: grey[200],
       borderRadius: 10,
-      margin: 5,
       flexGrow: 1,
     },
     active: {
       backgroundColor: theme.palette.primary.main,
+    },
+    button: {
+      borderRadius: theme.spacing(1),
+      textTransform: 'none',
+      width: '100%',
+      '&:hover': {
+        backgroundColor: '#FFF',
+      },
     },
   })
 )
