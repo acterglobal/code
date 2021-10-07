44f2f7afbd9e5283ddf46552e9dad9fbaef2a378
malik <malik.shaik@outlook.com>
2021-10-07 12:50:27 +0200

    fixed notifications data update issue

--- packages/lib/notification/use-notifications.ts
+++ packages/lib/notification/use-notifications.ts
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 
+import { useRouter } from 'next/router'
+
 import { ApolloError, useLazyQuery } from '@apollo/client'
 
 import {
@@ -9,7 +11,7 @@ import {
 import { useUser } from '@acter/lib/user/use-user'
 import GET_NOTIFICATIONS from '@acter/schema/queries/get-new-notifications-by-user.graphql'
 
-type UseFetchNotificationsQueryResults = {
+type UseNotificationsQueryResults = {
   notifications: Notifications
   loading: boolean
   error: ApolloError
@@ -18,18 +20,15 @@ type UseFetchNotificationsQueryResults = {
  * Gives notification info for new posts/activities/members on a specific acter
  * @returns object with count of new posts/activities/members
  */
-export const useFetchNotifications = (): UseFetchNotificationsQueryResults => {
+export const useNotifications = (): UseNotificationsQueryResults => {
   const [notifications, setNotifications] = useState<Notifications>({})
   const { user } = useUser()
+  const { asPath: url } = useRouter()
 
   const [
     fetchNotifications,
-    { loading, error, ...restQueryResult },
-  ] = useLazyQuery(GET_NOTIFICATIONS, {
-    onCompleted: (data) => {
-      setNotifications(getNotificationsGroupByActer(data.notifications))
-    },
-  })
+    { data, loading, error, refetch, ...restQueryResult },
+  ] = useLazyQuery(GET_NOTIFICATIONS)
 
   useEffect(() => {
     if (user) {
@@ -38,6 +37,20 @@ export const useFetchNotifications = (): UseFetchNotificationsQueryResults => {
       })
     }
   }, [user])
+
+  useEffect(() => {
+    if (user) {
+      refetch({
+        variables: { toActer: user.Acter.id },
+      })
+    }
+  }, [url])
+
+  useEffect(() => {
+    if (data) {
+      setNotifications(getNotificationsGroupByActer(data.notifications))
+    }
+  }, [data])
 
   return { notifications, loading, error, ...restQueryResult }
 }
