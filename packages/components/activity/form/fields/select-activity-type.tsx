f01d6498c0ac11dd0075a3a2d435691725de981b
Malik <44589493+malik-shaik@users.noreply.github.com>
2021-10-07 17:03:30 +0200

    refactored (#397)

--- packages/components/activity/form/fields/select-activity-type.tsx
+++ packages/components/activity/form/fields/select-activity-type.tsx
@@ -1,14 +1,12 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
-import { Box } from '@material-ui/core'
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
-
-import clsx from 'clsx'
+import { createStyles, makeStyles, Theme } from '@material-ui/core'
+// TODO: refactor below import after update to mui-5
+import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab'
 
 import { LoadingSpinner } from '@acter/components/util/loading-spinner'
 import { useActivityTypes } from '@acter/lib/activity-types/use-activity-types'
 import { ActivityTypes } from '@acter/lib/constants'
-import { capitalize } from '@acter/lib/string/capitalize'
 
 export interface SelectActivityTypeProps {
   selectedTypeId: string
@@ -19,77 +17,60 @@ export const SelectActivityType: FC<SelectActivityTypeProps> = ({
   selectedTypeId,
   onChange,
 }) => {
-  const classes = useStyles()
   const { activityTypes, loading } = useActivityTypes()
-
   if (loading) return <LoadingSpinner />
   if (!activityTypes) return null
 
+  const [color, setColor] = useState<ActivityTypes>()
+
+  const classes = useStyles({ color })
+
+  useEffect(() => {
+    const activityType = activityTypes.find(
+      (type) => type.id === selectedTypeId
+    )
+    setColor(activityType.name as ActivityTypes)
+  }, [selectedTypeId])
+
   return (
-    <Box className={classes.root}>
-      {activityTypes.map((type, i) => {
+    <ToggleButtonGroup
+      className={classes.buttons}
+      value={selectedTypeId}
+      exclusive
+      onChange={(e, id) => onChange(id)}
+    >
+      {activityTypes.map((type) => {
         if (type.name !== ActivityTypes.MEETING) {
           return (
-            <Box
-              key={`activity-type-${type.id}`}
-              className={clsx(
-                classes.button,
-                classes[`button${i}`],
-                selectedTypeId === type.id && classes[type.name]
-              )}
-              onClick={() => onChange(type.id)}
+            <ToggleButton
+              className={classes.button}
+              value={type.id}
+              key={type.id}
             >
-              {capitalize(type.name)}
-            </Box>
+              {type.name}
+            </ToggleButton>
           )
         }
       })}
-    </Box>
+    </ToggleButtonGroup>
   )
 }
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
+    buttons: {
       width: '100%',
-      display: 'flex',
       marginBottom: theme.spacing(2),
-    },
-    button: {
-      flexGrow: 1,
-      padding: theme.spacing(0.5),
-      border: '1px solid',
-      borderColor: theme.palette.secondary.light,
-      borderRadius: 4,
-      textAlign: 'center',
-      '&:hover': {
-        cursor: 'pointer',
+      '& .MuiToggleButton-root.Mui-selected': {
+        backgroundColor: ({ color }: { color: ActivityTypes }) =>
+          theme.colors.activityTypes[color],
+        borderColor: ({ color }: { color: ActivityTypes }) =>
+          theme.colors.activityTypes[color],
       },
     },
-    button0: {
-      borderTopRightRadius: 0,
-      borderBottomRightRadius: 0,
-    },
-    button1: {
-      borderRight: 0,
-      borderLeft: 0,
-      borderRadius: 0,
-    },
-    button2: {
-      borderTopLeftRadius: 0,
-      borderBottomLeftRadius: 0,
-    },
-    event: {
-      border: 0,
-      backgroundColor: theme.colors.activityTypes.event,
-    },
-    project: {
-      border: 0,
-      backgroundColor: theme.colors.activityTypes.project,
-    },
-    idea: {
-      border: 0,
-      backgroundColor: theme.colors.activityTypes.idea,
+    button: {
+      width: '33.39%',
+      color: theme.colors.grey.main,
     },
   })
 )
