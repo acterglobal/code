e7bc477c8b17482086352e415e07b6caf4b4efd7
Jude Musyoki <judemusyoki@gmail.com>
2021-10-19 14:48:30 +0200

    added privacy policy

--- packages/components/layout/cookie-bar.tsx
+++ packages/components/layout/cookie-bar.tsx
@@ -1,10 +1,28 @@
 import React, { FC } from 'react'
 import CookieConsent from 'react-cookie-consent'
 
+import Link from 'next/link'
+
+import { makeStyles, Theme } from '@material-ui/core/styles'
+
 export const CookieBar: FC = () => {
+  const classes = useStyles()
   return (
-    <CookieConsent style={{ zIndex: 1300 }}>
+    <CookieConsent debug={true} style={{ zIndex: 1300 }}>
       This website uses cookies to enhance the user experience.
+      <Link href={'/privacy-policy'}>
+        <a className={classes.link}>
+          {' '}
+          You can click here to read our Privacy Policy.
+        </a>
+      </Link>
     </CookieConsent>
   )
 }
+
+const useStyles = makeStyles((theme: Theme) => ({
+  link: {
+    color: theme.colors.white,
+    textDecoration: 'none',
+  },
+}))
