82e4b755d283775f0a2c796982851421e63ddd3b
malik <malik.shaik@outlook.com>
2021-10-20 21:11:32 +0200

    invalid urls redirects to 404

--- packages/web/pages/[acterType]/[slug]/index.tsx
+++ packages/web/pages/[acterType]/[slug]/index.tsx
@@ -4,13 +4,23 @@ import { GetServerSideProps } from 'next'
 
 import { NextPageWithLayout } from 'pages/_app'
 
+import { UrlActerTypes } from '@acter/lib/constants'
+
+const { NETWORKS, ORGANISATIONS } = UrlActerTypes
+
 export const ActerLandingPage: NextPageWithLayout = () => <></>
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { acterType, slug } = params
+  const destination = [NETWORKS, ORGANISATIONS].includes(
+    acterType as UrlActerTypes
+  )
+    ? `/${acterType}/${slug}/forum`
+    : '/404'
   return {
     props: {},
     redirect: {
-      destination: `/${params.acterType}/${params.slug}/forum`,
+      destination,
     },
   }
 }
