7d7f79aa73ec6d676eb6d5409c22dc09a937ae03
Damon <damon@acter.global>
2021-11-02 14:57:46 +0100

    Fix broken datetime tests (#443)
    * Fix broken datetime tests
    
    * Adjust for server time being different

--- packages/lib/datetime/__tests__/parse-and-format.test.ts
+++ packages/lib/datetime/__tests__/parse-and-format.test.ts
@@ -1,20 +1,28 @@
-import { parseAndFormat } from '../parse-and-format'
+import { DATE_FORMAT_SHORT_TZ } from '@acter/lib/constants'
+import { getZonedFormattedDate } from '@acter/lib/datetime/get-zoned-formatted-date'
+import { parseAndFormat } from '@acter/lib/datetime/parse-and-format'
+import { parseDateOrString } from '@acter/lib/datetime/parse-date-or-string'
 
-import { DATE_FORMAT_SHORT } from '@acter/lib/constants'
+jest.mock('@acter/lib/datetime/parse-date-or-string')
+jest.mock('@acter/lib/datetime/get-zoned-formatted-date')
 
 describe('parseAndFormat', () => {
-  it('should format an ISO date string', () => {
-    expect(parseAndFormat('2021-09-06T20:45:00+1:00', DATE_FORMAT_SHORT)).toBe(
-      '6 Sep. 20:45'
-    )
+  const parseDateOrStringMock = parseDateOrString as jest.Mock
+  const getZonedFormattedDateMock = getZonedFormattedDate as jest.Mock
+
+  beforeEach(() => {
+    jest.resetAllMocks()
+    parseDateOrStringMock.mockReturnValue(new Date())
+    getZonedFormattedDateMock.mockReturnValue(() => '')
   })
 
-  it('should format a date', () => {
-    // Handle tz offset
-    const now = new Date()
-    const mins = 45 - now.getTimezoneOffset()
-    expect(
-      parseAndFormat(new Date(2021, 8, 6, 20, mins, 0), DATE_FORMAT_SHORT)
-    ).toBe('6 Sep. 20:45')
+  it('should call parseDateOrString', () => {
+    const d = parseAndFormat('2021-11-02T14:00:00', DATE_FORMAT_SHORT_TZ)
+    expect(parseDateOrStringMock).toBeCalledTimes(1)
+  })
+
+  it('should calll getZonedFormattedDate', () => {
+    const d = parseAndFormat('2021-11-02T14:00:00', DATE_FORMAT_SHORT_TZ)
+    expect(getZonedFormattedDateMock).toBeCalledTimes(1)
   })
 })
