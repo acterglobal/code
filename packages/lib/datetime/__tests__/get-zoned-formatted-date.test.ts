a161f714c0923c66b0b5ccd9b16e51194a7d6158
Damon <damon@vestervand.net>
2021-11-02 14:15:08 +0100

    Fix broken datetime tests

--- packages/lib/datetime/__tests__/get-zoned-formatted-date.test.ts
+++ packages/lib/datetime/__tests__/get-zoned-formatted-date.test.ts
@@ -1,15 +1,22 @@
 import { getZonedFormattedDate } from '../get-zoned-formatted-date'
 import { parseISO } from 'date-fns'
 
-import { DATE_FORMAT_SHORT } from '@acter/lib/constants'
+import { DATE_FORMAT_SHORT_TZ } from '@acter/lib/constants'
 
 describe('getZonedFormattedDate', () => {
   it('should handle timezone offsets', () => {
-    const parsedDate = parseISO('2021-09-06T20:45:00+1:00')
+    const parsedDate = parseISO('2021-09-06T20:45:00')
 
     // Watch that this doesn't shift with timezones
-    expect(getZonedFormattedDate(DATE_FORMAT_SHORT)(parsedDate)).toBe(
-      '6 Sep. 20:45'
+    expect(getZonedFormattedDate(DATE_FORMAT_SHORT_TZ)(parsedDate)).toBe(
+      '6 Sep. 18:45 GMT+2'
+    )
+  })
+
+  it('should handle direct date input', () => {
+    const testDate = new Date(2021, 8, 6, 20, 45, 0)
+    expect(getZonedFormattedDate(DATE_FORMAT_SHORT_TZ)(testDate)).toBe(
+      '6 Sep. 18:45 GMT+2'
     )
   })
 })
--- packages/lib/datetime/get-zoned-formatted-date.ts
+++ packages/lib/datetime/get-zoned-formatted-date.ts
@@ -3,8 +3,8 @@ import { format, addMinutes } from 'date-fns'
 export const getZonedFormattedDate = (formatString: string) => (
   date: Date
 ): string => {
-  const timeZoneOffSetMinutes = date?.getTimezoneOffset()
-  const zonedDate = addMinutes(date, timeZoneOffSetMinutes)
+  const dateTimeZoneOffsetMinutes = date?.getTimezoneOffset()
+  const zonedDate = addMinutes(date, dateTimeZoneOffsetMinutes)
   const formattedDate = format(zonedDate, formatString)
 
   return formattedDate
