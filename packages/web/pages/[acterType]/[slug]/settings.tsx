cfc427d32adc4b10fb21318dda78a9f80283348a
Jude <52712074+judemusyoki@users.noreply.github.com>
2021-11-15 09:45:57 +0100

    removed server side props (#454)

--- packages/web/pages/[acterType]/[slug]/settings.tsx
+++ packages/web/pages/[acterType]/[slug]/settings.tsx
@@ -1,45 +1,43 @@
 import React, { useEffect, useState } from 'react'
 
-import { withUrqlClient } from 'next-urql'
+import { useRouter } from 'next/router'
 
 import { NextPageWithLayout } from 'pages/_app'
-import {
-  checkRole,
-  getActer,
-  getActerTypes,
-  setActerType,
-  getLinks,
-  getUserProfile,
-} from 'props'
 
 import { ActerLayout } from '@acter/components/acter/layout'
 import { ActerSettings } from '@acter/components/acter/settings'
 import { Head } from '@acter/components/layout/head'
 import { useActer } from '@acter/lib/acter/use-acter'
 import { useUpdateActer } from '@acter/lib/acter/use-update-acter'
-import {
-  composeProps,
-  ComposedGetServerSideProps,
-} from '@acter/lib/compose-props'
 import { useLinks } from '@acter/lib/links/use-links'
-import { getUrqlClientOptions } from '@acter/lib/urql'
+import { useUser } from '@acter/lib/user/use-user'
+import { userHasRoleOnActer } from '@acter/lib/user/user-has-role-on-acter'
 import { ActerConnectionRole } from '@acter/schema'
 
 export const ActerSettingsPage: NextPageWithLayout = () => {
+  const router = useRouter()
   const baseTitle = 'Settings - Acter'
   const [title, setTitle] = useState(baseTitle)
   const [fetching, setLoading] = useState(false)
+  const { user, fetching: userLoading } = useUser()
   const { acter, fetching: getActerLoading } = useActer()
   const { links, fetching: linksLoading } = useLinks()
   const [{ fetching: updateActerLoading }, updateActer] = useUpdateActer(acter)
 
   useEffect(() => {
-    setLoading(getActerLoading || linksLoading || updateActerLoading)
-  }, [getActerLoading, linksLoading || updateActerLoading])
+    setLoading(
+      userLoading || getActerLoading || linksLoading || updateActerLoading
+    )
+  }, [userLoading, getActerLoading, linksLoading || updateActerLoading])
 
   useEffect(() => {
     if (acter?.name) setTitle(`${acter.name} - ${baseTitle}`)
   }, [acter])
+
+  if (!acter || !user) return null
+
+  const isAdmin = userHasRoleOnActer(user, ActerConnectionRole.ADMIN, acter)
+  !isAdmin && router.push('/401')
 
   return (
     <>
@@ -57,15 +55,4 @@ export const ActerSettingsPage: NextPageWithLayout = () => {
 
 ActerSettingsPage.getLayout = (page) => <ActerLayout>{page}</ActerLayout>
 
-export const getServerSideProps: ComposedGetServerSideProps = (ctx) =>
-  composeProps(
-    ctx,
-    getUserProfile(true),
-    getActerTypes,
-    setActerType,
-    getActer,
-    checkRole(ActerConnectionRole.ADMIN),
-    getLinks
-  )
-
-export default withUrqlClient(getUrqlClientOptions)(ActerSettingsPage)
+export default ActerSettingsPage
