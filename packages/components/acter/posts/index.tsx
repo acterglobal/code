bdc8826f57dce30e65f0146bdbc60d66773108f6
malik <malik.shaik@outlook.com>
2021-11-19 03:29:49 +0100

    added invite and join buttons on group header section

--- packages/components/acter/posts/index.tsx
+++ packages/components/acter/posts/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react'
 
-import { Grid, makeStyles, createStyles } from '@material-ui/core'
+import { Grid, makeStyles, createStyles, Theme } from '@material-ui/core'
 
 import { InfoSection } from '@acter/components/acter/landing-page/info-section'
 import { LandingPageLayout } from '@acter/components/acter/landing-page/layout'
@@ -11,7 +11,7 @@ export const ActerPosts: FC = () => {
 
   return (
     <LandingPageLayout>
-      <Grid className={classes.main} item xs={12} sm={12} md={8} xl={10}>
+      <Grid className={classes.posts} item xs={12} sm={12} md={8} xl={10}>
         <PostList />
       </Grid>
       <Grid className={classes.info} item xs={12} sm={12} md={4} xl={2}>
@@ -21,9 +21,13 @@ export const ActerPosts: FC = () => {
   )
 }
 
-const useStyles = makeStyles(
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    main: {
+    posts: {
+      '&.MuiGrid-item': {
+        paddingRight: theme.spacing(10),
+        paddingLeft: theme.spacing(4),
+      },
       order: 1,
     },
     info: {
