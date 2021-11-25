847eddfdead8be08093ffe76a3acbeafc31506d4
Damon <damon@vestervand.net>
2021-11-25 10:20:06 +0100

    [hotfix] Migration typo

--- packages/schema/prisma/migrations/20211118105124_add_who_can_join_acter/migration.sql
+++ packages/schema/prisma/migrations/20211118105124_add_who_can_join_acter/migration.sql
@@ -1,5 +1,7 @@
 -- CreateEnum
 CREATE TYPE "ActerWhoCanJoinSettings" AS ENUM ('USERS', 'ACTERS');
-
 -- AlterTable
-ALTER TABLE "acters" ADD COLUMN     "acter_who_can_join_setting" "ActerWhoCanJoinSettings" NOT NULL DEFAULT E'ACTERS';
+ALTER TABLE
+  "acters"
+ADD
+  COLUMN "acter_who_can_join_setting" "ActerWhoCanJoinSettings" NOT NULL DEFAULT 'ACTERS';
