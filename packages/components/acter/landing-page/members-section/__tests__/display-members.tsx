be77b2e49bd2e06ae5b0d2f2e3725e8f03443aef
malik <malik.shaik@outlook.com>
2021-11-25 08:55:40 +0100

    Fix DisplayMembers test by mocking useUpdateActerConnection, update textContent expectation

--- packages/components/acter/landing-page/members-section/__tests__/display-members.tsx
+++ packages/components/acter/landing-page/members-section/__tests__/display-members.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { DisplayMembers } from '@acter/components/acter/landing-page/members-section/display-members'
 import { useActer } from '@acter/lib/acter/use-acter'
+import { useUpdateActerConnection } from '@acter/lib/acter/use-update-connection'
 import { render, screen } from '@acter/lib/test-utils'
 import { useUser } from '@acter/lib/user/use-user'
 import {
@@ -11,14 +12,20 @@ import {
 } from '@acter/schema/fixtures'
 
 jest.mock('@acter/lib/acter/use-acter')
+jest.mock('@acter/lib/acter/use-update-connection')
 jest.mock('@acter/lib/user/use-user')
 
 describe('<DisplayMembers>', () => {
   const mockUseActer = useActer as jest.Mock
   const mockUseUser = useUser as jest.Mock
+  const useUpdateActerConnectionMock = useUpdateActerConnection as jest.Mock
   beforeEach(() => {
     mockUseActer.mockReturnValue({ acter: ExampleActer })
     mockUseUser.mockReturnValue({ user: ExampleUser })
+    useUpdateActerConnectionMock.mockReturnValue([
+      { fetching: false },
+      () => void 0,
+    ])
   })
 
   it('should display when there are no acters', () => {
@@ -30,7 +37,7 @@ describe('<DisplayMembers>', () => {
     const connections = [
       {
         ...ExampleActerConnection,
-        Follower: { ...ExampleActer, name: 'org1' },
+        Follower: { ...ExampleActer, name: 'org1', location: 'test location' },
       },
       {
         ...ExampleActerConnection,
@@ -53,6 +60,6 @@ describe('<DisplayMembers>', () => {
     render(<DisplayMembers followers={connections} />)
     const rows = await screen.findAllByRole('listitem')
     expect(rows).toHaveLength(3)
-    expect(rows[0].textContent).toEqual('org1organisation')
+    expect(rows[0].textContent).toEqual('org1test location')
   })
 })
