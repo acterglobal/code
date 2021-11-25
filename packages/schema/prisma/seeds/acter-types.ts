97696cb80e68998e88fa583ba4bf59f5ad019c42
malik <malik.shaik@outlook.com>
2021-11-25 20:03:48 +0100

    fixed acter type seeds

--- packages/schema/prisma/seeds/acter-types.ts
+++ packages/schema/prisma/seeds/acter-types.ts
@@ -5,7 +5,7 @@ import { ActerTypes } from '@acter/lib/constants'
 export const seedActerTypes = async (prisma: PrismaClient): Promise<void> => {
   // First loop is to create the ActerTypes
   await Promise.all(
-    Object.keys(ActerTypes).map(async (name) => {
+    Object.values(ActerTypes).map(async (name) => {
       const res = prisma.acterType.upsert({
         where: { name },
         create: { name },
