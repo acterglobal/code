8af4d079f0197da446c52967bb45d2e92ab483ac
Jude Musyoki <judemusyoki@gmail.com>
2021-11-30 13:21:11 +0100

    fixed migration stack

--- packages/schema/prisma/migrations/20211129105116_acter_who_can_join_setting_to_array/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `acter_who_can_join_setting` on the `acters` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "acters" DROP COLUMN "acter_who_can_join_setting",
-ADD COLUMN     "acter_who_can_join_settings" "ActerWhoCanJoinSettings"[];
--- packages/schema/prisma/migrations/20211129210356_add_acter_who_can_join_setting/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateEnum
-CREATE TYPE "ActerWhoCanJoinSettings" AS ENUM ('ALL', 'PEOPLE', 'ACTERS');
-
--- AlterTable
-ALTER TABLE "acters" ADD COLUMN     "acter_who_can_join_setting" "ActerWhoCanJoinSettings" NOT NULL DEFAULT E'ACTERS';
-
--- DropEnum
-DROP TYPE "ActerWhoCanJoinSetting";
