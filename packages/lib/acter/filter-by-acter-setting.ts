d05e4871f6ea576712b639cc4d4e9421cdddf0ad
Damon <damon@vestervand.net>
2021-12-01 13:43:59 +0100

    [hotfix] Don't use .tsx in packages/lib, with file

---
+++ packages/lib/acter/filter-by-acter-setting.ts
@@ -0,0 +1,33 @@
+import { getActersCanJoin } from '@acter/lib/acter/get-acters-can-join'
+import { ActerTypes } from '@acter/lib/constants/acter-types'
+import { Acter, ActerWhoCanJoinSettings } from '@acter/schema'
+
+const { ALL, ACTERS } = ActerWhoCanJoinSettings
+
+export const filterConnectionsByActerSetting = (
+  acter: Acter,
+  connections: Acter[]
+): Acter[] => {
+  const actersCanJoin = getActersCanJoin(acter)
+
+  const getFilteredConnections = (
+    connections: Acter[],
+    type: ActerWhoCanJoinSettings
+  ) => {
+    const filteredActers = connections.filter((acter) =>
+      type === ACTERS
+        ? acter.ActerType.name !== ActerTypes.USER
+        : acter.ActerType.name === ActerTypes.USER
+    )
+    return filteredActers
+  }
+
+  if (actersCanJoin !== ALL) {
+    const filteredConnections = getFilteredConnections(
+      connections,
+      actersCanJoin
+    )
+    return filteredConnections
+  }
+  return connections
+}
