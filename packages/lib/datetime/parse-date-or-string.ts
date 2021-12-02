0340ac9d93c2477a002933027162b18ff91d567f
Damon <damon@vestervand.net>
2021-12-02 19:56:27 +0100

    Remove console.log, add eslint rule

--- packages/lib/datetime/parse-date-or-string.ts
+++ packages/lib/datetime/parse-date-or-string.ts
@@ -1,8 +1,5 @@
 import { parseJSON } from 'date-fns'
 
 export const parseDateOrString = (dateString: Date | string): Date => {
-  const res =
-    typeof dateString === 'string' ? parseJSON(dateString) : dateString
-  console.log('Parsing %s of %s to %s', typeof dateString, dateString, res)
-  return res
+  return typeof dateString === 'string' ? parseJSON(dateString) : dateString
 }
