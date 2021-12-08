fc9a25a0ff1b15b5c54f515e33a82cefa0ac3f06
malik <malik.shaik@outlook.com>
2021-12-08 09:57:42 +0100

    added post reaction model

---
+++ packages/schema/prisma/migrations/20211208084112_add_post_reactions/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "PostReaction" (
+    "id" TEXT NOT NULL,
+    "emoji" TEXT NOT NULL,
+    "emojiUnicode" TEXT NOT NULL,
+    "postId" TEXT NOT NULL,
+    "acterId" TEXT NOT NULL,
+    "createdByUserId" TEXT NOT NULL,
+
+    CONSTRAINT "PostReaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "reactionForPost" ON "PostReaction"("emojiUnicode", "postId", "acterId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PostReaction_emojiUnicode_postId_acterId_key" ON "PostReaction"("emojiUnicode", "postId", "acterId");
+
+-- AddForeignKey
+ALTER TABLE "PostReaction" ADD CONSTRAINT "PostReaction_postId_fkey" FOREIGN KEY ("postId") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PostReaction" ADD CONSTRAINT "PostReaction_acterId_fkey" FOREIGN KEY ("acterId") REFERENCES "acters"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PostReaction" ADD CONSTRAINT "PostReaction_createdByUserId_fkey" FOREIGN KEY ("createdByUserId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
