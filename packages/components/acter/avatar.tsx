be2a786aceaa7607710e8f22f42d856db254fa5e
Jude Musyoki <judemusyoki@gmail.com>
2021-12-08 01:09:30 +0100

    cleaned up acter avatar styling

--- packages/components/acter/avatar.tsx
+++ packages/components/acter/avatar.tsx
@@ -8,8 +8,6 @@ import {
   Theme,
 } from '@material-ui/core'
 
-import clsx from 'clsx'
-
 import { getInitials } from '@acter/lib/get-initials'
 import { Acter } from '@acter/schema'
 
@@ -19,42 +17,8 @@ const useStyles = makeStyles((theme: Theme) => {
       width: ({ size }: { size: number }) => theme.spacing(size),
       height: ({ size }: { size: number }) => theme.spacing(size),
       fontSize: '100%',
-      backgroundColor: theme.colors.white,
-      color: theme.palette.secondary.dark,
     },
-    user: {
-      color: theme.palette.secondary.dark,
-      backgroundColor: theme.colors.white,
-    },
-    group: {
-      color: theme.palette.secondary.dark,
-      backgroundColor: theme.colors.white,
-    },
-    organisation: {
-      color: theme.palette.secondary.dark,
-      backgroundColor: theme.colors.white,
-    },
-    network: {
-      color: theme.palette.secondary.dark,
-      backgroundColor: theme.colors.white,
-    },
-    community: {
-      color: theme.palette.secondary.dark,
-      backgroundColor: theme.colors.white,
-    },
-    ngo: {
-      color: theme.palette.secondary.dark,
-      backgroundColor: theme.colors.white,
-    },
-    company: {
-      color: theme.palette.secondary.dark,
-      backgroundColor: theme.colors.white,
-    },
-    university: {
-      color: theme.palette.secondary.dark,
-      backgroundColor: theme.colors.white,
-    },
-    'public-organisation': {
+    acterAvatar: {
       color: theme.palette.secondary.dark,
       backgroundColor: theme.colors.white,
     },
@@ -80,13 +44,7 @@ export const ActerAvatar: FC<ActerAvatarProps> = ({ acter, size = 6 }) => {
   return (
     <Avatar
       size={size}
-      className={clsx(
-        acter.avatarUrl
-          ? null
-          : classes[
-              acter.ActerType.name.replace(/\s+/g, '-').toLocaleLowerCase()
-            ]
-      )}
+      className={acter.avatarUrl ? null : classes.acterAvatar}
       alt={`${acter.ActerType.name} ${acter.name}`}
       src={avatarUrl}
     >
