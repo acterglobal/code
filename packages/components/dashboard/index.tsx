0e4e7eaf5d5ddc2ce31208a1e7de8790d9028338
malik <malik.shaik@outlook.com>
2021-12-29 12:27:20 +0100

    added translations to dashboard

--- packages/components/dashboard/index.tsx
+++ packages/components/dashboard/index.tsx
@@ -13,6 +13,7 @@ import {
 
 import { DashboardContent } from '@acter/components/dashboard/content'
 import { HomeIcon } from '@acter/components/icons/home-icon'
+import { useTranslation } from '@acter/lib/i18n/use-translation'
 
 export const Dashboard: FC = () => {
   const classes = useStyles()
