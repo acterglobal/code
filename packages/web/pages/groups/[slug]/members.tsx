12dae506630de24485d3a0e2a82c4770c3d12bee
Damon <damon@vestervand.net>
2022-01-04 14:37:27 +0100

    [hotfix] Fix missed path change

--- packages/web/pages/groups/[slug]/members.tsx
+++ packages/web/pages/groups/[slug]/members.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 import { NextPageWithLayout } from 'pages/_app'
 
+import { Head } from '@acter/components/atoms/head'
 import { GroupLayout } from '@acter/components/group/layout/overall'
 import { GroupMembers } from '@acter/components/group/members'
-import { Head } from '@acter/components/layout/head'
 import { useActer } from '@acter/lib/acter/use-acter'
 
 export const GroupMembersPage: NextPageWithLayout = () => {
