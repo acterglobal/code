f6770d9ca1f44194e7f5fe9f808494e78b571dde
Jude Musyoki <judemusyoki@gmail.com>
2022-01-05 22:32:32 +0100

    protected forum/posts page

--- packages/components/errors/not-authorized.tsx
+++ packages/components/errors/not-authorized.tsx
@@ -7,7 +7,7 @@ import { Link } from '@acter/components/util/anchor-link'
 export const NotAuthorizedMessage: FC = () => {
   return (
     <Typography variant="body2" style={{ textAlign: 'center', paddingTop: 40 }}>
-      You are not authorized to view this page. Please{' '}
+      Oops, looks like you are not authorized to view this page. Please{' '}
       <Link href="/dashboard">click here</Link> to go to your dashboard.
     </Typography>
   )
--- packages/web/pages/[acterType]/[slug]/forum/index.tsx
+++ packages/web/pages/[acterType]/[slug]/forum/index.tsx
@@ -2,28 +2,34 @@ import { NextPageWithLayout } from 'pages/_app'
 
 import { ActerPosts } from '@acter/components/acter/posts'
 import { Head } from '@acter/components/atoms/head'
-import { NotLoggedInMessage, Custom401 } from '@acter/components/errors'
+import { Custom401 } from '@acter/components/errors'
 import { ActerLayout } from '@acter/components/layout/acter'
 import { LoadingSpinner } from '@acter/components/util/loading-spinner'
+import { checkMemberAccess } from '@acter/lib/acter/check-member-access'
+import { getPrivacySettings } from '@acter/lib/acter/get-privacy-settings'
+import { useActer } from '@acter/lib/acter/use-acter'
 import { useActerTitle } from '@acter/lib/acter/use-title'
 import { useUser } from '@acter/lib/user/use-user'
 
 export const ActerPostsPage: NextPageWithLayout = () => {
   const { title } = useActerTitle('forum')
+  const { acter } = useActer()
   const { user, fetching } = useUser()
 
   if (fetching) return <LoadingSpinner />
 
+  const isPrivate = getPrivacySettings(acter)
+
+  const isMember = checkMemberAccess(user, acter)
+
+  if (!user) return <Custom401 />
+
+  if (user && isPrivate && !isMember) return <Custom401 user={user} isPrivate />
+
   return (
     <>
-      {!user ? (
-        <Custom401 />
-      ) : (
-        <>
-          <Head title={title} />
-          <ActerPosts />
-        </>
-      )}
+      <Head title={title} />
+      <ActerPosts />
     </>
   )
 }
