3cd858102138a08432d8e069148a41b2f7ae5ab2
Jude Musyoki <judemusyoki@gmail.com>
2022-01-19 00:00:06 +0100

    fixed group authenication

--- packages/components/group/index.tsx
+++ packages/components/group/index.tsx
@@ -10,41 +10,38 @@ import {
   Theme,
 } from '@material-ui/core'
 
+import { GraphQLError } from 'graphql'
+
 import { LandingPageLayout } from '@acter/components/group/layout'
 import { DescriptionSection } from '@acter/components/group/sections/description'
 import { LinksSection } from '@acter/components/group/sections/links'
 import { MembersSection } from '@acter/components/group/sections/members'
 import { UpcomingActivities } from '@acter/components/group/sections/upcoming-activities'
 import { PostList } from '@acter/components/posts'
-import { LoadingSpinner } from '@acter/components/util/loading-spinner'
-import { checkMemberAccess } from '@acter/lib/acter/check-member-access'
-import { getPrivacySettings } from '@acter/lib/acter/get-privacy-settings'
 import { useActer } from '@acter/lib/acter/use-acter'
-import { useUser } from '@acter/lib/user/use-user'
+import {
+  NotAuthorizedError,
+  NotLoggedError,
+} from '@acter/lib/errors/graphql-errors'
 
 export const GroupLanding: FC = () => {
   const classes = useStyles()
   const router = useRouter()
-  const { acter, fetching: acterLoading } = useActer()
-
-  const { user, fetching: userLoading } = useUser()
-
-  const isPrivate = getPrivacySettings(acter)
-
-  const isMember = checkMemberAccess(user, acter)
+  const { error: acterError } = useActer()
 
   useEffect(() => {
-    if (!acterLoading && !userLoading) {
-      if (!user && isPrivate) router.push('/401')
-      if (!acter) router.push('/404')
-
-      if (user && acter) {
-        if (isPrivate && !isMember) router.push('/403')
-      }
+    if (acterError) {
+      const { graphQLErrors }: GraphQLError[] | any = acterError
+      graphQLErrors.forEach((err) => {
+        if (err.message === NotAuthorizedError.message) {
+          router.push('/403')
+        }
+        if (err.message === NotLoggedError.message) {
+          router.push('/401')
+        }
+      })
     }
-  }, [acterLoading, acter, userLoading, user])
-
-  if (acterLoading || userLoading) return <LoadingSpinner />
+  }, [acterError])
 
   return (
     <LandingPageLayout>
