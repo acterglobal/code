dc9c2243c01ec093191d56f758bccdad235ab39a
Jude Musyoki <judemusyoki@gmail.com>
2022-01-18 19:26:05 +0100

    added error redirects to settings page

--- packages/web/pages/[acterType]/[slug]/settings.tsx
+++ packages/web/pages/[acterType]/[slug]/settings.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 import { useRouter } from 'next/router'
 
+import { GraphQLError } from 'graphql'
 import { NextPageWithLayout } from 'pages/_app'
 
 import { ActerSettings } from '@acter/components/acter/settings'
@@ -10,6 +11,10 @@ import { ActerLayout } from '@acter/components/layout/acter'
 import { LoadingSpinner } from '@acter/components/util/loading-spinner'
 import { getPrivacySettings } from '@acter/lib/acter/get-privacy-settings'
 import { useActer } from '@acter/lib/acter/use-acter'
+import {
+  NotAuthorizedError,
+  NotLoggedError,
+} from '@acter/lib/errors/graphql-errors'
 import { useUser } from '@acter/lib/user/use-user'
 import { userHasRoleOnActer } from '@acter/lib/user/user-has-role-on-acter'
 import { ActerConnectionRole } from '@acter/schema'
@@ -18,27 +23,36 @@ export const ActerSettingsPage: NextPageWithLayout = () => {
   const router = useRouter()
   const baseTitle = 'Settings - Acter'
   const [title, setTitle] = useState(baseTitle)
+  const { acter, fetching: acterLoading, error: acterError } = useActer()
   const { user, fetching: userLoading } = useUser()
-  const { acter, fetching: acterLoading } = useActer()
 
   useEffect(() => {
     if (acter?.name) setTitle(`${acter.name} - ${baseTitle}`)
   }, [acter])
 
+  const isPrivate = getPrivacySettings(acter)
   const isAdmin = userHasRoleOnActer(user, ActerConnectionRole.ADMIN, acter)
 
-  const isPrivate = getPrivacySettings(acter)
+  useEffect(() => {
+    if (acterError) {
+      const { graphQLErrors }: GraphQLError[] | any = acterError
+      graphQLErrors.forEach((err) => {
+        if (err.message === NotAuthorizedError.message) {
+          router.push('/403')
+        }
+        if (err.message === NotLoggedError.message) {
+          router.push('/401')
+        }
+      })
+    }
+  }, [acterError])
 
   useEffect(() => {
-    if (!acterLoading && !userLoading) {
-      if (!user && isPrivate) router.push('/401')
-      if (!acter) router.push('/404')
-
-      if (user && acter) {
-        if (!isAdmin) router.push('/403')
-      }
+    console.log('Is admin...', isAdmin)
+    if (isPrivate && !isAdmin) {
+      router.push('/403')
     }
-  }, [acterLoading, acter, userLoading, user])
+  }, [isAdmin, user, isPrivate])
 
   if (acterLoading || userLoading) return <LoadingSpinner />
 
