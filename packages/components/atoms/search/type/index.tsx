bbfa3e8477c4dd7fd0e060e42bcbcdd148366fee
Damon <damon@vestervand.net>
2022-01-26 19:33:54 +0100

    Move SearchType to atoms/search/type

---
+++ packages/components/atoms/search/type/index.tsx
@@ -0,0 +1,75 @@
+import React, { FC } from 'react'
+
+import {
+  Box,
+  createStyles,
+  makeStyles,
+  Theme,
+  Typography,
+} from '@material-ui/core'
+
+import { Switch } from '@acter/components/atoms/fields/switch'
+import { Size } from '@acter/lib/constants'
+import { capitalize } from '@acter/lib/string/capitalize'
+import { pluralize } from '@acter/lib/string/pluralize'
+
+export interface SubtypeSelectProps {
+  subTypeName: string
+  checked: boolean
+  showTypeIcon?: boolean
+  onChange: (checked: boolean) => void
+}
+
+export const SubtypeSelect: FC<SubtypeSelectProps> = ({
+  subTypeName,
+  checked,
+  showTypeIcon: useTypeColorDot = false,
+  onChange,
+}) => {
+  const type: string = subTypeName
+  const classes = useStyles({ type })
+
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.type}>
+        {useTypeColorDot && <Box className={classes.icon}></Box>}
+        <Typography className={classes.typeName} variant="body2">
+          {pluralize(capitalize(subTypeName))}
+        </Typography>
+      </Box>
+      <Switch
+        name={subTypeName}
+        size={Size.SMALL}
+        checked={checked}
+        onChange={onChange}
+      />
+    </Box>
+  )
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginBottom: theme.spacing(1.5),
+    },
+    type: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    typeName: {
+      marginLeft: theme.spacing(2),
+      fontWeight: theme.typography.fontWeightLight,
+      fontSize: theme.spacing(1.5),
+    },
+    icon: {
+      height: 12,
+      width: 12,
+      borderRadius: '50%',
+      backgroundColor: ({ type }: { type: string }) =>
+        type && theme.colors.activityTypes[type],
+    },
+  })
+)
