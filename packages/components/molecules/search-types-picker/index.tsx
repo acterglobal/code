655f534048f9beaffcfbc611f3f238c0ddf58549
Damon <damon@vestervand.net>
2022-01-26 19:33:54 +0100

    Move SearchType to atoms/search/type

--- packages/components/molecules/search-types-picker/index.tsx
+++ packages/components/molecules/search-types-picker/index.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react'
 
 import { Box, createStyles, makeStyles, Theme } from '@material-ui/core'
 
-import { Type } from '@acter/components/atoms/search-type'
+import { Type } from '@acter/components/atoms/search/type'
 import { useSearchVariables } from '@acter/components/contexts/search-variables'
 import { SearchType } from '@acter/lib/constants'
 import { useSearchType } from '@acter/lib/search/use-search-type'
