cb2d6b6247b8f5b67040a2ea2707803f031e989e
malik <malik.shaik@outlook.com>
2022-02-03 13:58:50 +0100

    refactor and used schema enum

--- packages/components/organisms/side-bar/index.tsx
+++ packages/components/organisms/side-bar/index.tsx
@@ -64,7 +64,6 @@ export const Sidebar: FC = () => {
       <Hidden smDown>
         <Divider />
         <Box className={classes.profileItem}>
-          <Locales />
           <ProfileButton />
         </Box>
       </Hidden>
