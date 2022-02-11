440dc0ef5efcf1bffe071305b2bcdc3bb35c69da
malik <malik.shaik@outlook.com>
2022-02-11 01:54:56 +0100

    added TODO

--- packages/web/i18n.js
+++ packages/web/i18n.js
@@ -1,7 +1,7 @@
 module.exports = {
   loadLocaleFrom: (lang, ns) =>
     import(`./locales/${lang.toLowerCase()}/${ns}.json`).then((m) => m.default),
-  locales: ['EN', 'DK'],
+  locales: ['EN', 'DK'], // TODO: use locales from DB enum
   defaultLocale: 'EN',
   pages: {
     '*': ['common'],
