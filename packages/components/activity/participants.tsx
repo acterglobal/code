22873ea3118df564d11f31e83ef28d728646a303
Jude Musyoki <judemusyoki@gmail.com>
2022-02-13 17:47:53 +0100

    added participants link

--- packages/components/activity/participants.tsx
+++ packages/components/activity/participants.tsx
@@ -1,22 +1,10 @@
 import React, { FC } from 'react'
 
-import { Box, Typography } from '@material-ui/core'
+import { Box, Button, Typography } from '@material-ui/core'
 import { makeStyles, Theme } from '@material-ui/core/styles'
 
 import { FollowersAvatars } from '@acter/components/acter/followers-avatars'
 import { Acter } from '@acter/schema'
-
-const useStyles = makeStyles((theme: Theme) => ({
-  container: {
-    backgroundColor: 'white',
-    padding: theme.spacing(2),
-    borderRadius: theme.spacing(1),
-  },
-  heading: {
-    fontWeight: 'bolder',
-    fontSize: '0.9rem',
-  },
-}))
 
 export type ParticipantsProps = {
   acter: Acter
@@ -26,11 +14,46 @@ export const Participants: FC<ParticipantsProps> = ({ acter }) => {
   const classes = useStyles()
 
   return (
-    <Box className={classes.container}>
-      <Typography className={classes.heading} variant="h6">
-        Participants
-      </Typography>
+    <Box className={classes.participants}>
+      <Box className={classes.headingContainer}>
+        <Typography className={classes.heading} variant="h6">
+          Participants
+        </Typography>
+        <Button className={classes.button}>See All Participants</Button>
+      </Box>
+
       <FollowersAvatars acter={acter} />
     </Box>
   )
 }
+
+const useStyles = makeStyles((theme: Theme) => ({
+  participants: {
+    backgroundColor: 'white',
+    padding: theme.spacing(2),
+    borderRadius: theme.spacing(1),
+  },
+  headingContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: theme.spacing(1),
+  },
+  heading: {
+    fontWeight: 'bolder',
+    fontSize: '0.9rem',
+  },
+  button: {
+    padding: 0,
+    marginLeft: theme.spacing(0.5),
+    textTransform: 'none',
+    textDecoration: 'underline',
+    fontSize: 12,
+    color: theme.colors.grey.main,
+    '&:hover': {
+      color: theme.colors.grey.dark,
+      textDecoration: 'underline',
+      backgroundColor: theme.colors.white,
+    },
+  },
+}))
