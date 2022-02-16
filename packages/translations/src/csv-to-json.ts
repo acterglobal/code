786cd003c574f3eecf67e131a2d7d2c160636c6e
Damon <damon@vestervand.net>
2022-02-16 21:30:04 +0100

    Script for converting interests csv into i18n

---
+++ packages/translations/src/csv-to-json.ts
@@ -0,0 +1,50 @@
+import csv from 'csvtojson'
+import { promises as fs } from 'fs'
+import path from 'path'
+
+type Language = Record<string, string>
+type LanguageMap = Record<string, Language>
+
+const languageMap: LanguageMap = {
+  'en-US': {},
+  'da-DK': {},
+}
+
+export const csvToJson = async (): Promise<void> => {
+  const csvPath = path.join(__dirname, '..', 'csv')
+  const files = await fs.readdir(csvPath)
+
+  await files.map(async (filename) => {
+    const file = await csv().fromFile(path.join(csvPath, filename))
+    const outFilename = filename.replace(
+      /Translations - ([^\\.]*).csv/,
+      '$1.json'
+    )
+
+    const translations = Object.keys(languageMap).reduce<LanguageMap>(
+      (memo, locale) => {
+        return {
+          ...memo,
+          [locale]: file.reduce(
+            (ts, t) => ({
+              ...ts,
+              [t['Translation Key']]: t[locale],
+            }),
+            {}
+          ),
+        }
+      },
+      languageMap
+    )
+
+    await Object.keys(translations).map(async (locale) => {
+      const folder = path.join(__dirname, '..', 'json', locale)
+      await fs.mkdir(folder, { recursive: true })
+      await fs.writeFile(
+        path.join(folder, outFilename),
+        Buffer.from(JSON.stringify(translations[locale], null, 2))
+      )
+    })
+  })
+}
+;(async () => csvToJson())()
