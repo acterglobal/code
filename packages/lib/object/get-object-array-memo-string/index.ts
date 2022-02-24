859c5e6c5f42bcb84f3c226953382988399a324d
Damon <damon@vestervand.net>
2022-02-24 21:36:14 +0100

    Remove debug logging

--- packages/lib/object/get-object-array-memo-string/index.ts
+++ packages/lib/object/get-object-array-memo-string/index.ts
@@ -1,9 +1,3 @@
 //eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/explicit-module-boundary-types
-export const getObjectArrayMemoString = (results: any): string => {
-  const out =
-    results && Array.isArray(results)
-      ? results.map((r) => r.id).join(',')
-      : null
-  console.log('Memoizing', results, out)
-  return out
-}
+export const getObjectArrayMemoString = (results: any): string =>
+  results && Array.isArray(results) ? results.map((r) => r.id).join(',') : null
