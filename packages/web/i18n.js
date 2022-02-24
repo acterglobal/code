a578db37e7fc2ad265b027890fb6d36aec4b8589
Damon <damon@vestervand.net>
2022-02-24 14:22:30 +0100

    Revert PR with a bad migration (#616)

--- packages/web/i18n.js
+++ packages/web/i18n.js
@@ -1,8 +1,8 @@
 module.exports = {
   loadLocaleFrom: (lang, ns) =>
-    import(`./locales/${lang}/${ns}.json`).then((m) => m.default),
-  locales: ['en-UK', 'da-DK'], // TODO: use locales from DB enum
-  defaultLocale: 'en-UK',
+    import(`./locales/${lang.toLowerCase()}/${ns}.json`).then((m) => m.default),
+  locales: ['EN', 'DK'], // TODO: use locales from DB enum
+  defaultLocale: 'EN',
   pages: {
     '*': ['common'],
     '/': ['common'],
