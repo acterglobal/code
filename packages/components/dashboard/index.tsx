3a89f28941af088ba52c614d23a92f16f5d4a5cd
malik <malik.shaik@outlook.com>
2022-03-01 17:53:06 +0100

    updated translation hook

--- packages/components/dashboard/index.tsx
+++ packages/components/dashboard/index.tsx
@@ -11,6 +11,7 @@ import {
 
 import { DashboardContent } from '@acter/components/dashboard/content'
 import { HomeIcon } from '@acter/components/icons/home-icon'
+import { useTranslation } from '@acter/lib/i18n/use-translation'
 
 export const Dashboard: FC = () => {
   const classes = useStyles()
