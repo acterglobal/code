600e9aa24c4fc76eaea655c1f3e5d90a6d723b91
malik <malik.shaik@outlook.com>
2022-03-01 23:09:33 +0100

    type fix

--- packages/lib/i18n/use-translation.ts
+++ packages/lib/i18n/use-translation.ts
@@ -1,4 +1,9 @@
-import { UseTranslationOptions, TFunction } from 'react-i18next'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  UseTranslationResponse,
+  UseTranslationOptions,
+  TFunction,
+} from 'react-i18next'
 
 import { useTranslation as useI18nTranslation } from 'next-i18next'
 
@@ -7,7 +12,7 @@ import { getTranslationKey } from '@acter/lib/i18n/get-translation-keys'
 export const useTranslation = (
   ns?: string | string[],
   options?: UseTranslationOptions<any>
-) => {
+): UseTranslationResponse<string | string[], any> => {
   const { t, ...rest } = useI18nTranslation(ns, options)
 
   const handleTranslation: TFunction = (i18nKey, options): string => {
