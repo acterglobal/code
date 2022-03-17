bc9e17eb22777b16406cbf0bab8aebed0b30779a
malik <malik.shaik@outlook.com>
2022-03-17 12:06:42 +0100

    added static paths & props to group landing page

--- packages/web/pages/groups/[slug]/index.tsx
+++ packages/web/pages/groups/[slug]/index.tsx
@@ -1,4 +1,8 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
 import { NextPageWithLayout } from 'pages/_app'
+import { getGroupLandingPageStaticPaths } from 'static-paths/group-landing-page-paths'
 
 import { Head } from '@acter/components/atoms/head'
 import { LoadingSpinner } from '@acter/components/atoms/loading/spinner'
@@ -22,5 +26,18 @@ export const GroupLandingPage: NextPageWithLayout = () => {
 }
 
 GroupLandingPage.getLayout = (page) => <GroupLayout>{page}</GroupLayout>
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, [
+      'common',
+      'interests',
+      'invitations',
+      'group-landing',
+    ])),
+  },
+})
+
+export const getStaticPaths: GetStaticPaths = getGroupLandingPageStaticPaths
 
 export default GroupLandingPage
