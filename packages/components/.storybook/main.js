8149edb16787619797f30c05558cd19347615a9e
Damon <damon@vestervand.net>
2022-03-22 14:58:46 +0100

    Use storybook-addon-swc to speed Storybook builds (#671)

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    'storybook-addon-swc',
     'storybook-formik/register',
     'storybook-addon-next-router',
     '@urql/storybook-addon',
