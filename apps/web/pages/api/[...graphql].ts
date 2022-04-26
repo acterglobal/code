df9092fb1cb4e5c496ace2a011ac645b6703eb15
Damon <damon@vestervand.net>
2022-04-26 22:03:56 +0200

    Move web to apps

--- apps/web/pages/api/[...graphql].ts
+++ apps/web/pages/api/[...graphql].ts
@@ -2,22 +2,46 @@ import 'reflect-metadata'
 
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import { getSession } from '@auth0/nextjs-auth0'
 import { withSentry } from '@sentry/nextjs'
 
-import { getApiHandler } from '@acter/api'
+import { ApolloServer } from 'apollo-server-micro'
+
+import { ActerGraphQLContext } from '@acter/lib/types/graphql-api'
+import { generateSchema } from '@acter/schema/generate-schema'
+import { prisma } from '@acter/schema/prisma'
 
 export const config = {
   api: {
     bodyParser: false,
-    externalResolver: true,
   },
 }
+
+let server: ApolloServer
 
 const graphqlHandler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
-  const handler = getApiHandler('/api/graphql')
+  // Only start the server once
+  if (!server) {
+    const schema = await generateSchema()
+    server = new ApolloServer({
+      schema,
+      context: ({ req, res }): ActerGraphQLContext => {
+        const session = getSession(req, res)
+        return {
+          session,
+          prisma,
+        }
+      },
+    })
+    await server.start()
+  } else {
+    console.debug('Apollo server already exists')
+  }
+
+  const handler = server.createHandler({ path: '/api/graphql' })
   return handler(req, res)
 }
 
