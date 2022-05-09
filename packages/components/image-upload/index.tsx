dfcc7dc0a56989901c9d47f539b16e20cfa363c6
Damon <damon@vestervand.net>
2022-05-09 14:39:57 +0200

    Don't log to Winston from components

--- packages/components/image-upload/index.tsx
+++ packages/components/image-upload/index.tsx
@@ -14,7 +14,6 @@ import { useFormikContext } from 'formik'
 
 import { ImageCropper } from '@acter/components/image-upload/cropper-editor'
 import { useTranslation } from '@acter/lib/i18n/use-translation'
-import { logger } from '@acter/lib/logger'
 
 registerPlugin(
   FilePondPluginImagePreview,
@@ -73,7 +72,7 @@ export const ImageUpload: FC<ImageUploadProps> = ({
     // Callback set by FilePond
     // - should be called by the editor when user closes the editor
     onclose: () => {
-      //   logger.log(pond.getFile())
+      //   console.log(pond.getFile())
     },
   })
 
@@ -146,7 +145,7 @@ export const ImageUpload: FC<ImageUploadProps> = ({
         )
         load(res.data)
       } catch (err) {
-        logger.error(err)
+        console.error(err)
         error(err)
       }
     },
