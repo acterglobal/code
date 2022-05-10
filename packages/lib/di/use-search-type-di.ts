9dd3e04b4e5edda313da17e5bcff0dd1e526e247
Damon <damon@vestervand.net>
2022-05-10 11:12:15 +0200

    Remove Magnetic DI (#727)

--- packages/lib/di/use-search-type-di.ts
+++
@@ -1,8 +0,0 @@
-import { injectable } from 'react-magnetic-di/macro'
-
-import { SearchType } from '../constants'
-import { useSearchType } from '../search/use-search-type'
-
-export const useSearchTypeDi = (
-  searchType = SearchType.ACTERS
-): typeof useSearchType => injectable(useSearchType, () => searchType)
