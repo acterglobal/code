6cd28538371ecf0f9c36d5a1cc353e744ed785a5
Damon <damon@vestervand.net>
2022-05-10 22:33:19 +0200

    Remove BullMQ, env-cmd

--- packages/lib/bullmq/scheduler.ts
+++
@@ -1,8 +0,0 @@
-import { QueueScheduler } from 'bullmq'
-import { createRedisConnection as connection } from './connection'
-
-export const createScheduler = (name: string): QueueScheduler => {
-  return new QueueScheduler(name, {
-    connection,
-  })
-}
