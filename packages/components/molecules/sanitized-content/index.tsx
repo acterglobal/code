989fe24d48ad8e70ae01f412f4a29458913f38f8
Jude Musyoki <judemusyoki@gmail.com>
2022-05-11 21:28:47 +0200

    added sanitize filter

--- packages/components/molecules/sanitized-content/index.tsx
+++ packages/components/molecules/sanitized-content/index.tsx
@@ -2,19 +2,30 @@ import { ReactNode } from 'react'
 import SanitizedHTML from 'react-sanitized-html'
 
 import Markdown from 'markdown-to-jsx'
+import sanitizeHtml from 'sanitize-html'
 
 export const SantizedContent = (
   content: string & ReactNode,
   isMarkDown: boolean
 ): JSX.Element => {
+  const sanitizedConmponent = (
+    <SanitizedHTML
+      allowedAttributes={{ a: ['href'] }}
+      allowedTags={['p', 'b', 'i', 'em', 'strong', 'a']}
+      html={content}
+    ></SanitizedHTML>
+  )
+
+  const tester1 = sanitizeHtml(sanitizedConmponent.props.html, {
+    exclusiveFilter: function (frame) {
+      return frame.tag === 'br' && !frame.text.trim()
+    },
+  })
+
   if (content)
     return isMarkDown ? (
       <Markdown>{content}</Markdown>
     ) : (
-      <SanitizedHTML
-        allowedAttributes={{ a: ['href'] }}
-        allowedTags={['p', 'b', 'i', 'em', 'strong', 'a']}
-        html={content}
-      ></SanitizedHTML>
+      <Markdown>{tester1}</Markdown>
     )
 }
