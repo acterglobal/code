8efc7691a86845f544212d7dc89a8d18ccab0d57
Damon <damon@vestervand.net>
2022-05-18 09:26:08 +0200

    Enhanced logging on login

--- apps/web/pages/api/auth/login.ts
+++ apps/web/pages/api/auth/login.ts
@@ -14,11 +14,23 @@ const loginHandler: NextApiHandler = async (req, res) => {
       return
     }
     const redirectUrl = getAuthRedirectUrl(req)
+    l.debug(
+      {
+        VERCEL_URL: process.env.VERCEL_URL,
+        BASE_URL: process.env.BASE_URL,
+        NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+        AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
+        AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
+        AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+        redirectUrl,
+      },
+      'Processing login with'
+    )
     await handleLogin(req, res, {
       returnTo: redirectUrl,
     })
   } catch (err) {
-    l.error('Failed in loginHandler:', err)
+    l.error('Login failed', err)
     res.status?.(err.status || 500).end(err.message)
   }
 }
