5974965239caeb52576eaf5169e9d427e9996216
Damon <damon@vestervand.net>
2022-05-23 14:14:06 +0200

    Handle login errors better
     - Don't 500 on Auth0/Intercom/Acter info sync
     - Log better error messages

--- apps/web/pages/api/auth/[...auth0].ts
+++ apps/web/pages/api/auth/[...auth0].ts
@@ -1,10 +1,15 @@
 import { handleAuth, handleCallback, AfterCallback } from '@auth0/nextjs-auth0'
 
+import { getLogger } from '@acter/../packages/lib/logger'
 import { getOrCreateActerFromDB } from '@acter/lib/acter/get-or-create-acter-from-db'
 import { getOrCreateUserByEmailFromDB } from '@acter/lib/user/get-or-create-user-by-email-from-db'
 import { syncAuth0IntercomData } from '@acter/lib/user/sync-auth0-intercom-data'
+import { User } from '@acter/schema'
+
+const t = getLogger('Auth0::afterCallback').startTimer()
 
 const afterCallback: AfterCallback = async (_req, res, session, _state) => {
+  let userWithActer: User
   try {
     // TODO: check for email_verified
     // TODO: don't implicitly link accounts
@@ -15,21 +20,26 @@ const afterCallback: AfterCallback = async (_req, res, session, _state) => {
     })
     const acter = await getOrCreateActerFromDB({ user, nickname })
 
-    const userWithActer = {
+    userWithActer = {
       ...user,
       Acter: acter,
-      sub: session.user.sub,
     }
+  } catch (error) {
+    t.done({
+      msg: 'error logging in',
+      error,
+      level: 'error',
+    })
+    throw error
+  }
 
-    syncAuth0IntercomData({ session, user })
+  // This function will log its own errors and *should not* result in a 500
+  syncAuth0IntercomData({ session, user: userWithActer })
 
-    return {
-      ...session,
-      user: userWithActer,
-    }
-  } catch (err) {
-    res.status(err.status || 500)
-    res.send(err.message)
+  t.done({ level: 'debug', msg: 'user logged in' })
+  return {
+    ...session,
+    user: userWithActer,
   }
 }
 
@@ -38,8 +48,7 @@ export default handleAuth({
     try {
       await handleCallback(req, res, { afterCallback })
     } catch (error) {
-      res.status(error.status || 500)
-      res.send(error.message)
+      res.status(error.status || 500).send(error.message)
     }
   },
 })
