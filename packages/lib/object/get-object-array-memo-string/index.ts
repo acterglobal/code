6e05fd9e16acd6a0faf138e50268b0049fa09425
Damon <damon@vestervand.net>
2022-06-09 16:18:21 +0200

    Remove unused getObjectArrayMemoString

--- packages/lib/object/get-object-array-memo-string/index.ts
+++
@@ -1,3 +0,0 @@
-//eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/explicit-module-boundary-types
-export const getObjectArrayMemoString = (results: any): string =>
-  results && Array.isArray(results) ? results.map((r) => r.id).join(',') : null
