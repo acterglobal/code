fe7b75440e80e84514d6d9cbaee31e5a1b882167
Damon <damon@vestervand.net>
2022-06-29 13:03:46 +0200

    [hotfix] remove console.log()

--- packages/components/molecules/fields/datetime-picker/index.tsx
+++ packages/components/molecules/fields/datetime-picker/index.tsx
@@ -36,7 +36,6 @@ export const DateTimePicker: FC<DateTimePickerProps> = (props) => {
   const { t } = useTranslation('common')
 
   const router = useRouter()
-  console.log('router', router)
   const locale = getDateTimeLocale(router.locale)
 
   const isValueValid = value && isValid(value)
