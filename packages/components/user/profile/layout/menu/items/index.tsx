ce257493b668d31537eb5d41e67b114317620a33
Birk Skyum <birk.skyum@pm.me>
2022-10-24 22:42:57 +0200

    Format for readability

--- packages/components/user/profile/layout/menu/items/index.tsx
+++ packages/components/user/profile/layout/menu/items/index.tsx
@@ -3,23 +3,28 @@ import React, { FC } from 'react'
 import { ProfileIcon, EditIcon, SettingsIcon } from '@acter/components/icons'
 import { ProfileMenuItem } from '@acter/components/user/profile/layout/menu/items/item'
 import { ProfileSettingsPages } from '@acter/lib/constants'
+import { useTranslation } from '@acter/lib/i18n/use-translation'
+import { capitalize } from '@acter/lib/string/capitalize'
 
 export const ProfileMenuItems: FC = () => {
+
+  const { t } = useTranslation();
+
   return (
     <>
       <ProfileMenuItem
         path={ProfileSettingsPages.INFO}
-        label="Profile View"
+        label={t('profileView')}
         Icon={ProfileIcon}
       />
       <ProfileMenuItem
         path={ProfileSettingsPages.EDIT}
-        label="Edit Profile"
+        label={t('profileEdit')}
         Icon={EditIcon}
       />
       <ProfileMenuItem
         path={ProfileSettingsPages.SETTINGS}
-        label="Profile settings"
+        label={capitalize(t('settings'))}
         Icon={SettingsIcon}
       />
     </>
